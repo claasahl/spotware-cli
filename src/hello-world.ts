0dc0b7151e9211b261c34795ffac80ff134a7c80
Claas Ahlrichs <claasahl@gmail.com>
2019-05-02 18:40:59 +0200

    imported spotware-adapter sample

--- src/hello-world.ts
+++ src/hello-world.ts
@@ -1,4 +1,46 @@
 import inquirer from "inquirer";
+import {
+  connect,
+  fromProtoMessage,
+  toProtoMessage,
+  writeProtoMessage
+} from "@claasahl/spotware-adapter";
+
+// establish connection
+const client = connect(
+  5035,
+  "live.ctraderapi.com"
+);
+
+// handle (incoming) proto messages
+client.on("PROTO_MESSAGE", (message, payloadType) => {
+  console.log(payloadType);
+  switch (payloadType) {
+    case "ERROR_RES": {
+      const msg = fromProtoMessage("ERROR_RES", message);
+      console.log(msg);
+      break;
+    }
+    case "PROTO_OA_VERSION_REQ": {
+      const msg = fromProtoMessage("PROTO_OA_VERSION_REQ", message);
+      console.log(msg);
+      break;
+    }
+    case "PROTO_OA_VERSION_RES": {
+      const msg = fromProtoMessage("PROTO_OA_VERSION_RES", message);
+      console.log(msg);
+      break;
+    }
+  }
+});
+
+// write (outgoing) proto messages
+setInterval(() => {
+  const message = toProtoMessage("HEARTBEAT_EVENT", {});
+  writeProtoMessage(client, message);
+}, 10000);
+
+writeProtoMessage(client, toProtoMessage("PROTO_OA_VERSION_REQ", {}));
 
 console.log("Hi, welcome to Node Pizza");
 
