7abe4471a30317859c5fd211b6e0728104275552
Claas Ahlrichs <claasahl@gmail.com>
2019-05-03 18:20:13 +0200

    implemented action: "configure server"

--- src/client.ts
+++ src/client.ts
@@ -2,13 +2,19 @@ import inquirer from "inquirer";
 import * as Rx from "rxjs";
 
 import STORE from "./store";
-import { connect, disconnect } from "./actions";
+import { configure, connect, disconnect } from "./actions";
 
 const mainMenu = {
   type: "list",
   name: "command",
   message: "What do you want to do?",
   choices: [
+    {
+      name: "configure server",
+      value: "configure",
+      disabled: () =>
+        STORE.getState().connected ? "already connected" : undefined
+    },
     {
       name: "connect to server",
       value: "connect",
@@ -31,6 +37,26 @@ const mainMenu = {
   ]
 };
 
+const askForHost = {
+  type: "list",
+  name: "host",
+  message: "To which host do you want to connect?",
+  choices: ["demo.ctraderapi.com", "live.ctraderapi.com"],
+  default: "live.ctraderapi.com"
+};
+const askForPort = {
+  type: "number",
+  name: "port",
+  message: "Which port should be connected to?",
+  validate: (value: number) => {
+    if (value > 0 && value < 65536) {
+      return true;
+    }
+    return "Please enter a valid port number";
+  },
+  default: 5035
+};
+
 const prompts = new Rx.Subject<inquirer.Question>();
 inquirer.prompt(prompts).ui.process.subscribe(
   next => {
@@ -40,6 +66,10 @@ inquirer.prompt(prompts).ui.process.subscribe(
           prompts.complete();
           unscribe();
           break;
+        case "configure":
+          prompts.next(askForHost);
+          prompts.next(askForPort);
+          break;
         case "connect":
           STORE.dispatch(connect());
           break;
@@ -47,6 +77,10 @@ inquirer.prompt(prompts).ui.process.subscribe(
           STORE.dispatch(disconnect());
           break;
       }
+    } else if (next.name === "host") {
+      STORE.dispatch(configure(next.answer, STORE.getState().port));
+    } else if (next.name === "port") {
+      STORE.dispatch(configure(STORE.getState().host, next.answer));
     }
   },
   error => console.log("error", error),
