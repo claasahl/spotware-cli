b266fd31e44b8bef0e666eaf374f85b679126234
Claas Ahlrichs <claasahl@gmail.com>
2019-05-04 14:46:49 +0200

    relocated spotware reducers

--- src/store.ts
+++ src/store.ts
@@ -3,10 +3,17 @@ import { createLogger } from "redux-logger";
 import thunkMiddleware from "redux-thunk";
 
 import { spotwareClient } from "./reducers";
+import { Actions } from "./actions";
+
+export interface State {
+  connected: boolean;
+  host: string;
+  port: number;
+}
 
 const loggerMiddleware = createLogger({ colors: false });
 
-const store = createStore(
+const store = createStore<State, Actions, {}, {}>(
   spotwareClient,
   applyMiddleware(thunkMiddleware, loggerMiddleware)
 );
