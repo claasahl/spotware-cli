c7892550d7bc3c2d92e23f1285e5c203f4d6551a
Claas Ahlrichs <claasahl@gmail.com>
2019-07-26 11:22:07 +0200

    added rxjs

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,23 @@
 import { EOL } from "os";
+import { fromEvent, of, pipe, EMPTY } from "rxjs";
+import { map, flatMap, scan, toArray } from "rxjs/operators";
 import ms from "ms";
 import fs from "fs";
-import {connect, write, ProtoOAPayloadType, ProtoPayloadType} from "@claasahl/spotware-adapter";
+import {
+  connect,
+  write,
+  ProtoOAPayloadType,
+  ProtoPayloadType,
+  ProtoOAQuoteType,
+  ProtoMessages
+} from "@claasahl/spotware-adapter";
 
 const socket = connect(
   5035,
   "demo.ctraderapi.com"
 );
 socket.on("PROTO_MESSAGE.*", message => {
-  if ([2113, 2115, 2154, 2161].includes(message.payloadType)) {
+  if ([2113, 2115, 2154, 2161, 2146].includes(message.payloadType)) {
     process.stdout.write(
       JSON.stringify({
         timestamp: new Date(),
@@ -22,16 +31,24 @@ socket.on("PROTO_MESSAGE.*", message => {
   }
 });
 socket.on("PROTO_MESSAGE.2113", message =>
-  fs.writeFileSync("./assets.json", JSON.stringify(message, null, 2), {encoding: "utf8"})
+  fs.writeFileSync("./assets.json", JSON.stringify(message, null, 2), {
+    encoding: "utf8"
+  })
 );
 socket.on("PROTO_MESSAGE.2115", message =>
-  fs.writeFileSync("./symbols.json", JSON.stringify(message, null, 2), {encoding: "utf8"})
+  fs.writeFileSync("./symbols.json", JSON.stringify(message, null, 2), {
+    encoding: "utf8"
+  })
 );
 socket.on("PROTO_MESSAGE.2154", message =>
-  fs.writeFileSync("./assetClasses.json", JSON.stringify(message, null, 2), {encoding: "utf8"})
+  fs.writeFileSync("./assetClasses.json", JSON.stringify(message, null, 2), {
+    encoding: "utf8"
+  })
 );
 socket.on("PROTO_MESSAGE.2161", message =>
-  fs.writeFileSync("./categories.json", JSON.stringify(message, null, 2), {encoding: "utf8"})
+  fs.writeFileSync("./categories.json", JSON.stringify(message, null, 2), {
+    encoding: "utf8"
+  })
 );
 socket.on("close", () => process.exit(0));
 
@@ -50,8 +67,8 @@ const accessToken = "";
 const refreshToken = "";
 const ctidTraderAccountId = 0;
 const symbolId = [1]; //,47,22395];
-const toTimestamp = Date.now();
-const fromTimestamp = toTimestamp - ms("7d");
+// const toTimestamp = Date.now();
+// const fromTimestamp = toTimestamp - ms("7d");
 write(socket, {
   payloadType: ProtoOAPayloadType.PROTO_OA_APPLICATION_AUTH_REQ,
   payload: { clientId, clientSecret }
@@ -134,3 +151,71 @@ setTimeout(() => {
     payload: { ctidTraderAccountId, symbolId }
   });
 }, 18000);
+
+const ASK = "PROTO_OA_GET_TICKDATA_REQ ASK 1";
+const BID = "PROTO_OA_GET_TICKDATA_REQ BID 1";
+const toTimestamp = new Date("2019-07-24T18:20:34.433Z").getTime(); //Date.now();
+const fromTimestamp = new Date("2019-07-24T18:20:01.361Z").getTime(); //toTimestamp - ms("1min");
+setTimeout(() => {
+  write(socket, {
+    payloadType: ProtoOAPayloadType.PROTO_OA_GET_TICKDATA_REQ,
+    payload: {
+      ctidTraderAccountId,
+      fromTimestamp,
+      toTimestamp,
+      symbolId: 1,
+      type: ProtoOAQuoteType.ASK
+    },
+    clientMsgId: ASK
+  });
+  write(socket, {
+    payloadType: ProtoOAPayloadType.PROTO_OA_GET_TICKDATA_REQ,
+    payload: {
+      ctidTraderAccountId,
+      fromTimestamp,
+      toTimestamp,
+      symbolId: 1,
+      type: ProtoOAQuoteType.BID
+    },
+    clientMsgId: BID
+  });
+}, 5000);
+// socket.on("PROTO_MESSAGE.2146", abc)
+const observable = fromEvent<ProtoMessages>(socket, "PROTO_MESSAGE.2146");
+observable;
+const a = observable.pipe(
+  extrapolateTickData(),
+  map(value => ({ ...value, date: new Date(value.timestamp) }))
+);
+a.subscribe(console.log);
+
+function filter2146() {
+  return pipe(
+    flatMap((value: ProtoMessages) => {
+      if (value.payloadType === 2146) {
+        return of(value);
+      }
+      return EMPTY;
+    })
+    // filter(value => value.payloadType === 2146),
+  );
+}
+
+function extrapolateTickData() {
+  return pipe(
+    filter2146(),
+    flatMap(value =>
+      of(...value.payload.tickData).pipe(
+        scan((acc, value) => {
+          return {
+            timestamp: acc.timestamp + value.timestamp,
+            tick: acc.tick + value.tick
+          };
+        }),
+        toArray(),
+        map(ticks => ticks.reverse()),
+        flatMap(value => of(...value))
+      )
+    )
+  );
+}
