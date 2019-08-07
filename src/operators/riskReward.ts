02ece8ee313ae633b64faed8511d95f2bd475087
Claas Ahlrichs <claasahl@gmail.com>
2019-08-07 11:18:26 +0200

    added duration to risk-reward operator

--- src/operators/riskReward.ts
+++ src/operators/riskReward.ts
@@ -1,28 +1,39 @@
 import { OperatorFunction, pipe } from "rxjs";
 import { scan, map } from "rxjs/operators";
+import ms = require("ms");
 
 import { Trendbar } from "../types";
 
 export interface RiskReward {
+  duration: string;
+  durationMS: number;
   risk: number;
   reward: number;
   ratio: number;
 }
 
 export function riskReward(
-  reference: number
+  referencePrice: number,
+  referenceTimestamp: number
 ): OperatorFunction<Trendbar, RiskReward> {
   return pipe(
     scan(
       (acc, curr) => ({
+        timestamp: curr.timestamp,
         reward: Math.max(acc.reward, curr.high),
         risk: Math.min(acc.risk, curr.low)
       }),
-      { risk: reference, reward: reference }
+      {
+        timestamp: referenceTimestamp,
+        risk: referencePrice,
+        reward: referencePrice
+      }
     ),
     map(value => ({
-      risk: value.risk - reference,
-      reward: value.reward - reference
+      duration: ms(value.timestamp - referenceTimestamp),
+      durationMS: value.timestamp - referenceTimestamp,
+      risk: value.risk - referencePrice,
+      reward: value.reward - referencePrice
     })),
     map(value => ({ ...value, ratio: Math.abs(value.reward / value.risk) }))
   );
