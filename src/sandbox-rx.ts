4180eaf036c6c3feabfaa0deedaec5d48eef0878
Claas Ahlrichs <claasahl@gmail.com>
2019-08-28 21:48:26 +0200

    take proto mesages until socket closes/errors

--- src/sandbox-rx.ts
+++ src/sandbox-rx.ts
@@ -1,39 +1,12 @@
 import * as $ from "@claasahl/spotware-adapter";
-import { Subject, fromEvent, throwError, race, EMPTY } from "rxjs";
-import { map, tap, share, first, flatMap } from "rxjs/operators";
+import { fromEvent, throwError, race, EMPTY } from "rxjs";
+import { first, flatMap, takeUntil, endWith } from "rxjs/operators";
 
 import config from "./config";
-import { throttle } from "./operators";
 
 const { host, port } = config;
 
 const socket = $.connect(port, host);
-
-const incomingProtoMessages = fromEvent<$.ProtoMessages>(
-  socket,
-  "PROTO_MESSAGE.*"
-).pipe(share());
-incomingProtoMessages
-  .pipe(
-    map(pm => {
-      const date = new Date();
-      return { timestamp: date.getTime(), date, msg: pm };
-    }),
-    tap(msg => console.log(JSON.stringify(msg)))
-  )
-  .subscribe();
-
-const outgoingProtoMessages = new Subject<$.ProtoMessages>();
-outgoingProtoMessages
-  .pipe(
-    throttle(300),
-    tap(pm => $.write(socket, pm))
-  )
-  .subscribe();
-
-export function output(pm: $.ProtoMessages) {
-  outgoingProtoMessages.next(pm);
-}
 
 const error = fromEvent<never>(socket, "error").pipe(
   first(),
@@ -47,8 +20,12 @@ const close = fromEvent<never>(socket, "close").pipe(
   first(),
   flatMap(() => EMPTY)
 );
-race(error, end, close).subscribe(
-  next => console.log("next", next),
-  error => console.log("error", error),
-  () => console.log("complete")
-);
+const endConditions = race(error, end, close).pipe(endWith("byebye"));
+const protoMessages = fromEvent<$.ProtoMessages>(socket, "PROTO_MESSAGE.*");
+protoMessages
+  .pipe(takeUntil(endConditions))
+  .subscribe(
+    next => console.log("next", next),
+    error => console.log("error", error),
+    () => console.log("complete")
+  );
