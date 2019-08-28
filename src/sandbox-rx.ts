7ae1fce2cc7a8e3dc84b7d3121f53d3de08abd34
Claas Ahlrichs <claasahl@gmail.com>
2019-08-28 21:54:24 +0200

    use "using"

--- src/sandbox-rx.ts
+++ src/sandbox-rx.ts
@@ -1,29 +1,37 @@
 import * as $ from "@claasahl/spotware-adapter";
-import { fromEvent, throwError, race, EMPTY } from "rxjs";
+import { fromEvent, throwError, race, EMPTY, using, timer } from "rxjs";
 import { first, flatMap, takeUntil, endWith } from "rxjs/operators";
 
 import config from "./config";
 
-const { host, port } = config;
+const protoMessages = using(
+  () => {
+    const { host, port } = config;
+    const socket = $.connect(port, host);
+    return { socket, unsubscribe: () => socket.end() };
+  },
+  resource => {
+    const { socket } = resource as any;
+    const error = fromEvent<never>(socket, "error").pipe(
+      first(),
+      flatMap(error => throwError(error))
+    );
+    const end = fromEvent<never>(socket, "end").pipe(
+      first(),
+      flatMap(() => EMPTY)
+    );
+    const close = fromEvent<never>(socket, "close").pipe(
+      first(),
+      flatMap(() => EMPTY)
+    );
+    const endConditions = race(error, end, close).pipe(endWith("byebye"));
+    const protoMessages = fromEvent<$.ProtoMessages>(socket, "PROTO_MESSAGE.*");
+    return protoMessages.pipe(takeUntil(endConditions));
+  }
+);
 
-const socket = $.connect(port, host);
-
-const error = fromEvent<never>(socket, "error").pipe(
-  first(),
-  flatMap(error => throwError(error))
-);
-const end = fromEvent<never>(socket, "end").pipe(
-  first(),
-  flatMap(() => EMPTY)
-);
-const close = fromEvent<never>(socket, "close").pipe(
-  first(),
-  flatMap(() => EMPTY)
-);
-const endConditions = race(error, end, close).pipe(endWith("byebye"));
-const protoMessages = fromEvent<$.ProtoMessages>(socket, "PROTO_MESSAGE.*");
 protoMessages
-  .pipe(takeUntil(endConditions))
+  .pipe(takeUntil(timer(1000)))
   .subscribe(
     next => console.log("next", next),
     error => console.log("error", error),
