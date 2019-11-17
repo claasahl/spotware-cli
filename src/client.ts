e01d52ad79828d62f8c76914c5796562ce93ef78
Claas Ahlrichs <claasahl@gmail.com>
2019-11-17 16:16:57 +0100

    keep track of spot prices

--- src/client.ts
+++ src/client.ts
@@ -1,10 +1,13 @@
 import { SpotwareSubject } from "./spotwareSubject";
-import ms from "ms";
 
 import config from "./config";
-import { applicationAuth, accountAuth, dealList } from "./requests";
-import { concat } from "rxjs";
-import { map } from "rxjs/operators";
+import { applicationAuth, accountAuth, subscribeSpots } from "./requests";
+import { concat, Subject } from "rxjs";
+import { map, filter, pairwise } from "rxjs/operators";
+import {
+  ProtoOAPayloadType,
+  ProtoMessage2131
+} from "@claasahl/spotware-adapter";
 
 const {
   port,
@@ -14,6 +17,7 @@ const {
   accessToken,
   ctidTraderAccountId
 } = config;
+const BTCEUR = 22396;
 const subject = new SpotwareSubject(port, host);
 
 subject
@@ -23,10 +27,49 @@ subject
   )
   .subscribe(console.log);
 
-const fromTimestamp = new Date("2019-11-11T00:00:00.0Z").getTime();
-const toTimestamp = fromTimestamp + ms("1w");
+interface Spot {
+  ask: number;
+  bid: number;
+  symbolId: number;
+  ctidTraderAccountId: number;
+  date: Date;
+}
+const spots = new Subject<Spot>();
+subject
+  .pipe(
+    filter(
+      (pm): pm is ProtoMessage2131 =>
+        pm.payloadType === ProtoOAPayloadType.PROTO_OA_SPOT_EVENT
+    ),
+    filter(pm => pm.payload.ctidTraderAccountId === ctidTraderAccountId),
+    filter(pm => !!(pm.payload.ask || pm.payload.bid)),
+    filter(pm => pm.payload.symbolId === BTCEUR), // <---
+    map(({ payload: { ask, bid, ctidTraderAccountId, symbolId } }) => ({
+      ask,
+      bid,
+      symbolId,
+      ctidTraderAccountId,
+      date: new Date()
+    })),
+    pairwise(),
+    map(([left, right]) => {
+      const spot = { ...right };
+      if (!spot.ask) {
+        spot.ask = left.ask;
+      }
+      if (!spot.bid) {
+        spot.bid = left.bid;
+      }
+      return spot;
+    }),
+    filter((spot): spot is Spot => !!(spot.ask && spot.bid))
+  )
+  .subscribe(spots);
+
+spots.subscribe(console.log);
+
 concat(
   applicationAuth(subject, { clientId, clientSecret }),
   accountAuth(subject, { accessToken, ctidTraderAccountId }),
-  dealList(subject, { ctidTraderAccountId, fromTimestamp, toTimestamp })
+  subscribeSpots(subject, { ctidTraderAccountId, symbolId: [BTCEUR] })
 ).subscribe();
