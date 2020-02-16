8b477a313ff0ff0a60a2af150d66f9cd7717fd7d
Claas Ahlrichs <claasahl@gmail.com>
2020-02-16 09:09:16 +0100

    switched from callbacks to events

--- src/services/spotware/index.ts
+++ src/services/spotware/index.ts
@@ -21,11 +21,7 @@ function isOAError(msg: $.ProtoMessages): msg is $.ProtoMessage2142 {
     return msg.payloadType === $.ProtoOAPayloadType.PROTO_OA_ERROR_RES
 }
 
-interface Callback<RES extends $.ProtoMessages> {
-    (err: $.ProtoMessage50 | $.ProtoMessage2142 | null, msg?: RES): void;
-}
-
-function authApplication(callback: Callback<$.ProtoMessage2101>) {
+function authApplication() {
     const msgId = clientMsgId();
     setImmediate(() => $.write(socket, {payloadType: $.ProtoOAPayloadType.PROTO_OA_APPLICATION_AUTH_REQ, payload: {clientId, clientSecret}, clientMsgId: msgId}));
     function isResponse(msg: $.ProtoMessages): msg is $.ProtoMessage2101 {
@@ -33,20 +29,18 @@ function authApplication(callback: Callback<$.ProtoMessage2101>) {
     }
     function response(msg: $.ProtoMessages) {
         if(msg.clientMsgId === msgId && isResponse(msg)) {
-            setImmediate(() => callback(null, msg))
+            setImmediate(() => socket.emit("authApplication", msg))
             socket.off("PROTO_MESSAGE.INPUT.*", response);
-        } else if(msg.clientMsgId === msgId && isError(msg)) {
-            setImmediate(() => callback(msg))
-            socket.off("PROTO_MESSAGE.INPUT.*", response);
-        } else if(msg.clientMsgId === msgId && isOAError(msg)) {
-            setImmediate(() => callback(msg))
+        } else if(msg.clientMsgId === msgId && (isError(msg) || isOAError(msg))) {
+            const {errorCode, description} = msg.payload
+            setImmediate(() => socket.emit("error", new Error(`${errorCode}, ${description}`)))
             socket.off("PROTO_MESSAGE.INPUT.*", response);
         }
     }
     socket.on("PROTO_MESSAGE.INPUT.*", response)
 }
 
-function lookupAccounts(callback: Callback<$.ProtoMessage2150>) {
+function lookupAccounts() {
     const msgId = clientMsgId();
     setImmediate(() => $.write(socket, {payloadType: $.ProtoOAPayloadType.PROTO_OA_GET_ACCOUNTS_BY_ACCESS_TOKEN_REQ, payload: {accessToken}, clientMsgId: msgId}));
     function isResponse(msg: $.ProtoMessages): msg is $.ProtoMessage2150 {
@@ -54,20 +48,18 @@ function lookupAccounts(callback: Callback<$.ProtoMessage2150>) {
     }
     function response(msg: $.ProtoMessages) {
         if(msg.clientMsgId === msgId && isResponse(msg)) {
-            setImmediate(() => callback(null, msg))
+            setImmediate(() => socket.emit("lookupAccounts", msg))
             socket.off("PROTO_MESSAGE.INPUT.*", response);
-        } else if(msg.clientMsgId === msgId && isError(msg)) {
-            setImmediate(() => callback(msg))
-            socket.off("PROTO_MESSAGE.INPUT.*", response);
-        } else if(msg.clientMsgId === msgId && isOAError(msg)) {
-            setImmediate(() => callback(msg))
+        } else if(msg.clientMsgId === msgId && (isError(msg) || isOAError(msg))) {
+            const {errorCode, description} = msg.payload
+            setImmediate(() => socket.emit("error", new Error(`${errorCode}, ${description}`)))
             socket.off("PROTO_MESSAGE.INPUT.*", response);
         }
     }
     socket.on("PROTO_MESSAGE.INPUT.*", response)
 }
 
-function authAccount(ctidTraderAccountId: number, callback: Callback<$.ProtoMessage2103>) {
+function authAccount(ctidTraderAccountId: number) {
     const msgId = clientMsgId();
     setImmediate(() => $.write(socket, {payloadType: $.ProtoOAPayloadType.PROTO_OA_ACCOUNT_AUTH_REQ, payload: {accessToken, ctidTraderAccountId}, clientMsgId: msgId}));
     function isResponse(msg: $.ProtoMessages): msg is $.ProtoMessage2103 {
@@ -75,13 +67,11 @@ function authAccount(ctidTraderAccountId: number, callback: Callback<$.ProtoMess
     }
     function response(msg: $.ProtoMessages) {
         if(msg.clientMsgId === msgId && isResponse(msg)) {
-            setImmediate(() => callback(null, msg))
+            setImmediate(() => socket.emit("authAccount", msg))
             socket.off("PROTO_MESSAGE.INPUT.*", response);
-        } else if(msg.clientMsgId === msgId && isError(msg)) {
-            setImmediate(() => callback(msg))
-            socket.off("PROTO_MESSAGE.INPUT.*", response);
-        } else if(msg.clientMsgId === msgId && isOAError(msg)) {
-            setImmediate(() => callback(msg))
+        } else if(msg.clientMsgId === msgId && (isError(msg) || isOAError(msg))) {
+            const {errorCode, description} = msg.payload
+            setImmediate(() => socket.emit("error", new Error(`${errorCode}, ${description}`)))
             socket.off("PROTO_MESSAGE.INPUT.*", response);
         }
     }
@@ -94,20 +84,6 @@ function heartbeat() {
 
 function connected() {
     log("connected")
-    authApplication((err, msg) => {
-        if(err) {
-            error("%s: %s", err.payload.errorCode, err.payload.description)
-        } else if(msg) {
-            lookupAccounts((err, msg) => {
-                if(err) {
-                    error("%s: %s", err.payload.errorCode, err.payload.description)
-                } else if(msg) {
-                    const {ctidTraderAccountId} = msg.payload.ctidTraderAccount[0]
-                    authAccount(ctidTraderAccountId, () => {})
-                }
-            })
-        }
-    })
 }
 
 function disconnected() {
@@ -122,6 +98,7 @@ const { port, host, clientId, clientSecret, accessToken } = config
 const socket = $.connect(port, host)
 socket.on("connect", connected)
 socket.on("close", disconnected)
+socket.on("error", (err: Error) => {error(err.message); socket.end();})
 socket.on("PROTO_MESSAGE.INPUT.*", (msg: $.ProtoMessages) => {
     input("%j", msg)
 })
@@ -132,3 +109,12 @@ socket.on("PROTO_MESSAGE.OUTPUT.*", (msg: $.ProtoMessages) => {
     }
     pacemaker = setTimeout(heartbeat, 10000)
 })
+
+socket.on("connect", authApplication)
+socket.on("authApplication", lookupAccounts)
+socket.on("lookupAccounts", (msg: $.ProtoMessage2150) => {
+    assert.strictEqual(msg.payload.ctidTraderAccount.length, 1);
+    const {ctidTraderAccountId}= msg.payload.ctidTraderAccount[0]
+    authAccount(ctidTraderAccountId)
+})
+socket.on("authAccount", () => {})
