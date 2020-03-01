6aaec37feed4efe9e3c73116c7489dcbbf1efee6
Claas Ahlrichs <claasahl@gmail.com>
2020-03-01 14:35:44 +0100

    refactored generator function

--- src/services/local/index.ts
+++ src/services/local/index.ts
@@ -4,87 +4,25 @@ import { DebugSpotPricesStream, SpotPricesStream, AskPriceChangedEvent, BidPrice
 import { Symbol, Currency, Price, Timestamp } from "../types";
 import { OrderStream, DebugOrderStream } from "../order";
 
-const samples: (AskPriceChangedEvent | BidPriceChangedEvent | PriceChangedEvent)[] = [
-    {
-        "ask": 6611.79,
-        "timestamp": 1577663999771
-    },
-    {
-        "bid": 6612.03,
-        "timestamp": 1577663999425
-    },
-    {
-        "bid": 6612.28,
-        "timestamp": 1577663999110
-    },
-    {
-        "ask": 6612.52,
-        "timestamp": 1577663998928
-    },
-    {
-        "bid": 6612.73,
-        "timestamp": 1577663998447
-    },
-    {
-        "bid": 6613.18,
-        "timestamp": 1577663998021
-    },
-    {
-        "ask": 6613.21,
-        "timestamp": 1577663997680
-    },
-    {
-        "bid": 6613.11,
-        "timestamp": 1577663997264
-    },
-    {
-        "bid": 6613.18,
-        "timestamp": 1577663997026
-    },
-    {
-        "bid": 6613.21,
-        "timestamp": 1577663996609
-    },
-    {
-        "ask": 6613.24,
-        "timestamp": 1577663995996
-    },
-    {
-        "bid": 6613.98,
-        "timestamp": 1577663995829
-    },
-    {
-        "bid": 6614.08,
-        "timestamp": 1577663995601
-    },
-    {
-        "ask": 6613,
-        "bid": 6613.91,
-        "timestamp": 1577663995198
-    },
-    {
-        "ask": 6613,
-        "bid": 6613.17,
-        "timestamp": 1577663994564
-    },
-    {
-        "ask": 6613,
-        "bid": 6613.14,
-        "timestamp": 1577663994182
-    },
-    {
-        "bid": 6613.21,
-        "timestamp": 1577663993987
-    },
-    {
-        "ask": 6612.72,
-        "timestamp": 1577663993516
-    }
-]
-function* test() {
-    for (const sample of samples) {
-        yield sample;
-    }
+function* test(): Generator<AskPriceChangedEvent | BidPriceChangedEvent | PriceChangedEvent, void, unknown> {
+    yield { "ask": 6611.79, "timestamp": 1577663999771 };
+    yield { "bid": 6612.03, "timestamp": 1577663999425 };
+    yield { "bid": 6612.28, "timestamp": 1577663999110 };
+    yield { "ask": 6612.52, "timestamp": 1577663998928 };
+    yield { "bid": 6612.73, "timestamp": 1577663998447 };
+    yield { "bid": 6613.18, "timestamp": 1577663998021 };
+    yield { "ask": 6613.21, "timestamp": 1577663997680 };
+    yield { "bid": 6613.11, "timestamp": 1577663997264 };
+    yield { "bid": 6613.18, "timestamp": 1577663997026 };
+    yield { "bid": 6613.21, "timestamp": 1577663996609 };
+    yield { "ask": 6613.24, "timestamp": 1577663995996 };
+    yield { "bid": 6613.98, "timestamp": 1577663995829 };
+    yield { "bid": 6614.08, "timestamp": 1577663995601 };
+    yield { "ask": 6613, "bid": 6613.91, "timestamp": 1577663995198 };
+    yield { "ask": 6613, "bid": 6613.17, "timestamp": 1577663994564 };
+    yield { "ask": 6613, "bid": 6613.14, "timestamp": 1577663994182 };
+    yield { "bid": 6613.21, "timestamp": 1577663993987 };
+    yield { "ask": 6612.72, "timestamp": 1577663993516 };
 }
 
 function emitSpotPrices(stream: DebugSpotPricesStream, e: AskPriceChangedEvent | BidPriceChangedEvent | PriceChangedEvent): void {
