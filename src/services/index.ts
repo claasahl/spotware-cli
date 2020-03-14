2c15cf5502c23b3a9ff36fa7e56f0c69b6c042a1
Claas Ahlrichs <claasahl@gmail.com>
2020-03-14 15:37:20 +0100

    updated notes

--- src/services/index.ts
+++ src/services/index.ts
@@ -10,11 +10,12 @@ export default {
 
 // Idea: Write services which consume events (from other services) and produce events (for other services to consume).
 
-// Services must only emit their "own" events... not events from other services
+// Services must only emit their "own" events... not events from other services (separation of concern)
 
 // Each service should have a Base implementation and at least a Debug imlementation (which allows emitting of all service-owned events)
 
 // each service should export a class that extends (and completes) its base implementation
+// alternatively, new services may be introduced through factory/creator-functions
 
 function local() {
   const name = "BTC/EUR";
