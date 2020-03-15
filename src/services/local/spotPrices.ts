dec8b2c4e353f296722786fab5b4d44e48b15b9d
Claas Ahlrichs <claasahl@gmail.com>
2020-03-15 11:33:12 +0100

    refined sample data

--- src/services/local/spotPrices.ts
+++ src/services/local/spotPrices.ts
@@ -18,24 +18,24 @@ async function* sampleData(): AsyncGenerator<
   void,
   unknown
 > {
-  yield { ask: 6611.79, timestamp: 1577663999771 };
-  yield { bid: 6612.03, timestamp: 1577663999425 };
-  yield { bid: 6612.28, timestamp: 1577663999110 };
-  yield { ask: 6612.52, timestamp: 1577663998928 };
-  yield { bid: 6612.73, timestamp: 1577663998447 };
-  yield { bid: 6613.18, timestamp: 1577663998021 };
-  yield { ask: 6613.21, timestamp: 1577663997680 };
-  yield { bid: 6613.11, timestamp: 1577663997264 };
-  yield { bid: 6613.18, timestamp: 1577663997026 };
-  yield { bid: 6613.21, timestamp: 1577663996609 };
-  yield { ask: 6613.24, timestamp: 1577663995996 };
-  yield { bid: 6613.98, timestamp: 1577663995829 };
-  yield { bid: 6614.08, timestamp: 1577663995601 };
-  yield { ask: 6613, bid: 6613.91, timestamp: 1577663995198 };
-  yield { ask: 6613, bid: 6613.17, timestamp: 1577663994564 };
-  yield { ask: 6613, bid: 6613.14, timestamp: 1577663994182 };
-  yield { bid: 6613.21, timestamp: 1577663993987 };
-  yield { ask: 6612.72, timestamp: 1577663993516 };
+  yield { timestamp: 1577663999771, ask: 6611.79 };
+  yield { timestamp: 1577663999425, bid: 6612.03 };
+  yield { timestamp: 1577663999110, bid: 6612.28 };
+  yield { timestamp: 1577663998928, ask: 6612.52 };
+  yield { timestamp: 1577663998447, bid: 6612.73 };
+  yield { timestamp: 1577663998021, bid: 6613.18 };
+  yield { timestamp: 1577663997680, ask: 6613.21 };
+  yield { timestamp: 1577663997264, bid: 6613.11 };
+  yield { timestamp: 1577663997026, bid: 6613.18 };
+  yield { timestamp: 1577663996609, bid: 6613.21 };
+  yield { timestamp: 1577663995996, ask: 6613.24 };
+  yield { timestamp: 1577663995829, bid: 6613.98 };
+  yield { timestamp: 1577663995601, bid: 6614.08 };
+  yield { timestamp: 1577663995198, ask: 6613, bid: 6613.91 };
+  yield { timestamp: 1577663994564, ask: 6613, bid: 6613.17 };
+  yield { timestamp: 1577663994182, ask: 6613, bid: 6613.14 };
+  yield { timestamp: 1577663993987, bid: 6613.21 };
+  yield { timestamp: 1577663993516, ask: 6612.72 };
 }
 
 async function* fr0mFile(
