6290a417470f89c808a4c8ca3870ec394c959e55
Claas Ahlrichs <claasahl@gmail.com>
2020-04-05 14:26:47 +0200

    tests for canceling previous orders

--- src/services/insideBarMomentumStrategy.test.js
+++ src/services/insideBarMomentumStrategy.test.js
@@ -105,4 +105,122 @@ describe("insideBarMomentumStrategy", () => {
 
         spotPrices.emitAsk({ timestamp: 1002000, ask: 0})
     })
+    test("cancel previous (BUY) order", done => {
+        const currency = Symbol.for("EUR");
+        const initialBalance = 1000;
+        const period = ms("1s");
+        const symbol = Symbol.for("BTC/EUR");
+        const enterOffset = 0.1;
+        const stopLossOffset = 0.4;
+        const takeProfitOffset = 0.8;
+        const minTrendbarRange = 15;
+        const volume = 0.1;
+        const spotPrices = new DebugSpotPricesStream({symbol})
+        const spots = () => spotPrices;
+        const account = insideBarMomentumStrategy({ currency, initialBalance, period, symbol, enterOffset, stopLossOffset, takeProfitOffset, minTrendbarRange, volume, spots })
+        
+        const orders = {
+            "1": [
+                {timestamp: expect.any(Number), status: "CREATED", id: "1", symbol, tradeSide: "BUY", volume, orderType: "STOP", enter: 15552, stopLoss: 15292, takeProfit: 15916},
+                {timestamp: expect.any(Number), status: "ACCEPTED", id: "1", symbol, tradeSide: "BUY", volume, orderType: "STOP", enter: 15552, stopLoss: 15292, takeProfit: 15916},
+                {timestamp: expect.any(Number), status: "CANCELED", id: "1", symbol, tradeSide: "BUY", volume, orderType: "STOP", enter: 15552, stopLoss: 15292, takeProfit: 15916}
+            ],
+            "2": [
+                {timestamp: expect.any(Number), status: "CREATED", id: "2", symbol, tradeSide: "SELL", volume, orderType: "STOP", enter: 14928, stopLoss: 15188, takeProfit: 14564},
+                {timestamp: expect.any(Number), status: "ACCEPTED", id: "2", symbol, tradeSide: "SELL", volume, orderType: "STOP", enter: 14928, stopLoss: 15188, takeProfit: 14564}
+            ]
+        }
+        account.on("order", e => {
+            if(orders[e.id] && orders[e.id].length > 0) {
+                expect(e).toStrictEqual(orders[e.id][0]);
+                orders[e.id].shift();
+                if(orders["1"].length === 0 && orders["2"].length === 0) {
+                    done();
+                }
+            } else {
+                fail(new Error(`unexpected order-event: ${JSON.stringify(e)}`))
+            }
+        })
+
+        spotPrices.emitBid({ timestamp: 1000000, bid: 15000})
+        spotPrices.emitBid({ timestamp: 1000250, bid: 15500})
+        spotPrices.emitBid({ timestamp: 1000500, bid: 14980})
+        spotPrices.emitBid({ timestamp: 1000750, bid: 15100})
+
+        spotPrices.emitBid({ timestamp: 1001000, bid: 15000})
+        spotPrices.emitBid({ timestamp: 1001250, bid: 15000})
+        spotPrices.emitBid({ timestamp: 1001500, bid: 15000})
+        spotPrices.emitBid({ timestamp: 1001750, bid: 15000})
+
+        spotPrices.emitBid({ timestamp: 1002000, bid: 15100})
+        spotPrices.emitBid({ timestamp: 1002250, bid: 15500})
+        spotPrices.emitBid({ timestamp: 1002500, bid: 14980})
+        spotPrices.emitBid({ timestamp: 1002750, bid: 15000})
+
+        spotPrices.emitBid({ timestamp: 1003000, bid: 15000})
+        spotPrices.emitBid({ timestamp: 1003250, bid: 15000})
+        spotPrices.emitBid({ timestamp: 1003500, bid: 15000})
+        spotPrices.emitBid({ timestamp: 1003750, bid: 15000})
+
+        spotPrices.emitAsk({ timestamp: 1004000, ask: 0})
+    })
+    test("cancel previous (SELL) order", done => {
+        const currency = Symbol.for("EUR");
+        const initialBalance = 1000;
+        const period = ms("1s");
+        const symbol = Symbol.for("BTC/EUR");
+        const enterOffset = 0.1;
+        const stopLossOffset = 0.4;
+        const takeProfitOffset = 0.8;
+        const minTrendbarRange = 15;
+        const volume = 0.1;
+        const spotPrices = new DebugSpotPricesStream({symbol})
+        const spots = () => spotPrices;
+        const account = insideBarMomentumStrategy({ currency, initialBalance, period, symbol, enterOffset, stopLossOffset, takeProfitOffset, minTrendbarRange, volume, spots })
+
+        const orders = {
+            "1": [
+                {timestamp: expect.any(Number), status: "CREATED", id: "1", symbol, tradeSide: "SELL", volume, orderType: "STOP", enter: 14928, stopLoss: 15188, takeProfit: 14564},
+                {timestamp: expect.any(Number), status: "ACCEPTED", id: "1", symbol, tradeSide: "SELL", volume, orderType: "STOP", enter: 14928, stopLoss: 15188, takeProfit: 14564},
+                {timestamp: expect.any(Number), status: "CANCELED", id: "1", symbol, tradeSide: "SELL", volume, orderType: "STOP", enter: 14928, stopLoss: 15188, takeProfit: 14564}
+            ],
+            "2": [
+                {timestamp: expect.any(Number), status: "CREATED", id: "2", symbol, tradeSide: "BUY", volume, orderType: "STOP", enter: 15552, stopLoss: 15292, takeProfit: 15916},
+                {timestamp: expect.any(Number), status: "ACCEPTED", id: "2", symbol, tradeSide: "BUY", volume, orderType: "STOP", enter: 15552, stopLoss: 15292, takeProfit: 15916}
+            ]
+        }
+        account.on("order", e => {
+            if(orders[e.id] && orders[e.id].length > 0) {
+                expect(e).toStrictEqual(orders[e.id][0]);
+                orders[e.id].shift();
+                if(orders["1"].length === 0 && orders["2"].length === 0) {
+                    done();
+                }
+            } else {
+                fail(new Error(`unexpected order-event: ${JSON.stringify(e)}`))
+            }
+        })
+
+        spotPrices.emitBid({ timestamp: 1000000, bid: 15100})
+        spotPrices.emitBid({ timestamp: 1000250, bid: 15500})
+        spotPrices.emitBid({ timestamp: 1000500, bid: 14980})
+        spotPrices.emitBid({ timestamp: 1000750, bid: 15000})
+
+        spotPrices.emitBid({ timestamp: 1001000, bid: 15000})
+        spotPrices.emitBid({ timestamp: 1001250, bid: 15000})
+        spotPrices.emitBid({ timestamp: 1001500, bid: 15000})
+        spotPrices.emitBid({ timestamp: 1001750, bid: 15000})
+
+        spotPrices.emitBid({ timestamp: 1002000, bid: 15000})
+        spotPrices.emitBid({ timestamp: 1002250, bid: 15500})
+        spotPrices.emitBid({ timestamp: 1002500, bid: 14980})
+        spotPrices.emitBid({ timestamp: 1002750, bid: 15100})
+
+        spotPrices.emitBid({ timestamp: 1003000, bid: 15000})
+        spotPrices.emitBid({ timestamp: 1003250, bid: 15000})
+        spotPrices.emitBid({ timestamp: 1003500, bid: 15000})
+        spotPrices.emitBid({ timestamp: 1003750, bid: 15000})
+
+        spotPrices.emitAsk({ timestamp: 1004000, ask: 0})
+    })
 })
