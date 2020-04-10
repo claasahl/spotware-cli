7e5dd6e5baee713b1d2d44df66021c6e8475fad3
Claas Ahlrichs <claasahl@gmail.com>
2020-04-10 17:21:43 +0200

    publisher + pacemaker

--- src/services/spotware/util.ts
+++ src/services/spotware/util.ts
@@ -1,6 +1,39 @@
 import * as $ from "@claasahl/spotware-adapter"
 import debug from "debug"
 
+interface Publisher {
+  publish: (msg: $.ProtoMessages) => void,
+  done: () => void
+}
+
+function publisher(socket: $.SpotwareSocket): Publisher {
+  const messages: $.ProtoMessages[] = [];
+  function publish() {
+    setImmediate(() => {
+      const msg = messages.shift();
+      if (msg) {
+        $.write(socket, msg);
+      }
+    });
+  }
+  const publisher: NodeJS.Timeout = setInterval(publish, 300)
+  return {
+    publish: (msg: $.ProtoMessages) => messages.push(msg),
+    done: () => clearInterval(publisher)
+  }
+}
+
+function pacemaker(socket: $.SpotwareSocket, publisher: Publisher) {
+  const heartbeat: $.ProtoMessages = { payloadType: $.ProtoPayloadType.HEARTBEAT_EVENT, payload: {} }
+  let pacemaker: NodeJS.Timeout | null = setTimeout(() => publisher.publish(heartbeat), 10000);
+  socket.on("PROTO_MESSAGE.OUTPUT.*", () => {
+    if (pacemaker) {
+      clearTimeout(pacemaker);
+    }
+    pacemaker = setTimeout(() => publisher.publish(heartbeat), 10000);
+  });
+}
+
 interface SockProps {
   port: number;
   host: string;
@@ -15,11 +48,14 @@ export function sock(props: SockProps): $.SpotwareSocket {
   const error = log.extend("error");
 
   const socket = $.connect(props.port, props.host);
+  const P = publisher(socket);
   socket.on("connect", () => {
-    log("connected")
+    log("connected");
+    pacemaker(socket, P);
   })
   socket.on("close", () => {
     log("disconnect")
+    P.done();
   })
   socket.on("error", (err: Error) => {
     error(err.message);
@@ -30,5 +66,6 @@ export function sock(props: SockProps): $.SpotwareSocket {
   socket.on("PROTO_MESSAGE.OUTPUT.*", msg => {
     output("%j", msg);
   });
+
   return socket;
 }
