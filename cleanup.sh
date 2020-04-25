78119acdab5488fcae78a017ec01eab214b1a8e4
Claas Ahlrichs <claasahl@gmail.com>
2020-04-25 13:11:48 +0200

    added new cleanup script

---
+++ cleanup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+DATE=`date '+%Y-%m-%d'`
+FILE="${DATE}.log"
+LOG_FILES="log.files"
+
+ls -1 \@* | grep -v ".tar.gz" | sort > ${LOG_FILES}
+cat ${LOG_FILES} | xargs cat >> ${FILE}
+tar -czf "${FILE}.tar.gz" ${FILE}
+cat ${LOG_FILES} | xargs rm -f
+rm -f ${LOG_FILES} ${FILE}
