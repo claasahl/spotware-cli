2515757ac91f760fa105cd1e9a2cc164b5ef68e1
Claas Ahlrichs <claasahl@gmail.com>
2020-04-26 15:30:25 +0200

    "replay" spot prices from log files

--- src/services/local/spotPrices.ts
+++ src/services/local/spotPrices.ts
@@ -9,7 +9,7 @@ const log = debug("local-data");
 
 class LocalSpotPricesStream extends B.DebugSpotPricesStream {
   trendbars(props: B.SpotPricesSimpleTrendbarsProps): Promise<B.TrendbarsStream> {
-    return trendbarsFromSpotPrices({...props, ...this.props, spots: this});
+    return trendbarsFromSpotPrices({ ...props, ...this.props, spots: this });
   }
 }
 
@@ -36,6 +36,44 @@ async function* sampleData(): AsyncGenerator<
   yield { timestamp: 1577663994182, ask: 6613, bid: 6613.14 };
   yield { timestamp: 1577663993987, bid: 6613.21 };
   yield { timestamp: 1577663993516, ask: 6612.72 };
+}
+
+async function* fr0mLogFiles(
+  paths: fs.PathLike[]
+): AsyncGenerator<
+  B.AskPriceChangedEvent | B.BidPriceChangedEvent | B.PriceChangedEvent,
+  void,
+  unknown
+> {
+  for (const path of paths) {
+    const fileStream = fs.createReadStream(path);
+
+    const rl = readline.createInterface({
+      input: fileStream,
+      crlfDelay: Infinity
+    });
+
+    for await (const line of rl) {
+      try {
+        if (!line.includes("spotPrices:ask") && !line.includes("spotPrices:bid")) {
+          continue;
+        }
+        const logEntry = /({.*})/.exec(line)
+        if (logEntry) {
+          const data = JSON.parse(logEntry[1]);
+          if (typeof data === "object") {
+            yield data;
+          }
+        }
+      } catch {
+        log(
+          "failed to parse line '%s' as JSON (%s)... skipping",
+          line,
+          path.toString()
+        );
+      }
+    }
+  }
 }
 
 async function* fr0mFile(
@@ -103,9 +141,29 @@ export function fromSampleData(props: B.SpotPricesProps): B.SpotPricesStream {
   return stream;
 }
 
-export function fromFile(props: B.SpotPricesProps & {path: fs.PathLike}): B.SpotPricesStream {
-  const {path, ...originalProps} = props;
+export function fromFile(props: B.SpotPricesProps & { path: fs.PathLike }): B.SpotPricesStream {
+  const { path, ...originalProps } = props;
   const data = fr0mFile(path);
+  const stream = new LocalSpotPricesStream(originalProps);
+  const emitNext = () => {
+    data.next().then(a => {
+      setImmediate(() => {
+        if (a.value) {
+          emitSpotPrices(stream, a.value);
+        }
+      })
+    });
+  }
+  setImmediate(() => {
+    stream.on("next", emitNext)
+    emitNext();
+  })
+  return stream;
+}
+
+export function fromLogFiles(props: B.SpotPricesProps & { paths: fs.PathLike[] }): B.SpotPricesStream {
+  const { paths, ...originalProps } = props;
+  const data = fr0mLogFiles(paths);
   const stream = new LocalSpotPricesStream(originalProps);
   const emitNext = () => {
     data.next().then(a => {
