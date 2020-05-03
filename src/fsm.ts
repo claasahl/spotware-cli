06cfa3d4cdc826c8527a87075c51882a69f21a9b
Claas Ahlrichs <claasahl@gmail.com>
2020-05-03 12:43:05 +0200

    tried package: finity

--- src/fsm.ts
+++ src/fsm.ts
@@ -1,99 +1,43 @@
-import { createMachine, interpret, StateMachine } from '@xstate/fsm';
-import * as B from "./services/base"
+import Finity from 'finity';
 
-
-
-type Context = {};
-
-type Event =
-  | { type: 'ACCEPT' }
-  | { type: 'REJECT' }
-  | { type: 'FILL' }
-  | { type: 'CLOSE' }
-  | { type: 'CANCEL' }
-  | { type: 'EXPIRE' }
-  | { type: 'END' };
-
-type State =
-  | { value: 'created', context: B.OrderCreatedEvent }
-  | { value: 'accepted', context: B.OrderAcceptedEvent }
-  | { value: 'rejected', context: B.OrderRejectedEvent }
-  | { value: 'filled', context: B.OrderFilledEvent }
-  | { value: 'closed', context: B.OrderClosedEvent }
-  | { value: 'canceled', context: B.OrderCanceledEvent }
-  | { value: 'expired', context: B.OrderExpiredEvent }
-  | { value: 'ended', context: B.OrderEndedEvent }
-
-const machine = createMachine<Context, Event, State>({
-  initial: "created",
-  states: {
-    created: {
-      on: {
-        ACCEPT: 'accepted',
-        REJECT: 'rejected',
-        CANCEL: 'canceled'
-      }
-    },
-    accepted: {
-      on: {
-        FILL: 'filled',
-        CANCEL: 'canceled',
-        EXPIRE: 'expired'
-      }
-    },
-    rejected: {
-      on: {
-        END: 'ended'
-      }
-    },
-    filled: {
-      on: {
-        CLOSE: 'closed'
-      }
-    },
-    closed: {
-      on: {
-        END: 'ended'
-      }
-    },
-    canceled: {
-      on: {
-        END: 'ended'
-      }
-    },
-    expired: {
-      on: {
-        END: 'ended'
-      }
-    },
-    ended: {
-    }
-  }
-});
-
-// console.log("----->", service.send({type: "END", cb: () => {}}));
-async function ksdjfh(service: StateMachine.Service<Context, Event, State>, type: Event['type'], goal: State['value']): Promise<void> {
-  service.send(type)
-  console.log(service.state.value)
-  if(service.state.changed) {
-      console.log("magic did happen (1)")
-  } else if(service.state.value === goal) {
-      console.log("magic did happen (2)")
-  }
-  throw new Error(`did not change state`)
+enum State {
+  created,
+  accepted,
+  filled,
+  rejected,
+  expired,
+  closed,
+  canceled
 }
-// async function ksdjfh(service: StateMachine.Service<Context, Event, State>, type: Event['type']): Promise<void> {
-//   await new Promise<void>((resolve, reject) => {
-//     service.send({ type, onSuccess: resolve, onError: reject })
-//   })
-//   console.log("magic did happen")
-// }
-async function main() {
-  const service = interpret(machine);
-  service.start();
-  await ksdjfh(service, "REJECT", "rejected"); console.log(2)
-  await ksdjfh(service, "REJECT", "rejected"); console.log(2)
-  await ksdjfh(service, "END", "ended"); console.log(2)
-  await ksdjfh(service, "END", "ended"); console.log(2)
+
+enum Event {
+  accept,
+  reject,
+  fill,
+  cancel,
+  close,
+  expire
 }
-main();
+
+const worker = Finity
+  .configure<State, Event>()
+    .initialState(State.created)
+      .on(Event.accept).transitionTo(State.accepted)
+      .on(Event.reject).transitionTo(State.rejected)
+    .state(State.accepted)
+      .on(Event.expire).transitionTo(State.expired)
+      .on(Event.cancel).transitionTo(State.canceled)
+      .on(Event.fill).transitionTo(State.filled)
+    .state(State.filled)
+      .on(Event.close).transitionTo(State.closed)
+    .state(State.closed)
+    .state(State.rejected)
+    .state(State.expired)
+    .state(State.canceled)
+    .global()
+      .onStateEnter(state => console.log(`Entering state '${State[state]}'`))
+  .start();
+
+worker.handle(Event.accept);
+// worker.handle(Event.expire);
+
