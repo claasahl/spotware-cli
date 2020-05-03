c78646554caa5d1bb0a8b794661ecf81cf0353cb
Claas Ahlrichs <claasahl@gmail.com>
2020-05-03 16:08:13 +0200

    added FSM to LocalOrderStream

--- src/services/local/order.ts
+++ src/services/local/order.ts
@@ -1,62 +1,180 @@
+import { createMachine, interpret, StateMachine } from '@xstate/fsm';
 import * as B from "../base"
 
+type Context = 
+| B.OrderCreatedEvent
+| B.OrderAcceptedEvent
+| B.OrderRejectedEvent
+| B.OrderFilledEvent
+| B.OrderClosedEvent
+| B.OrderCanceledEvent
+| B.OrderExpiredEvent
+
+type Event =
+| { type: 'CREATE', event: B.OrderCreatedEvent }
+| { type: 'ACCEPT', event: B.OrderAcceptedEvent }
+| { type: 'REJECT', event: B.OrderRejectedEvent }
+| { type: 'FILL', event: B.OrderFilledEvent }
+| { type: 'CLOSE', event: B.OrderClosedEvent }
+| { type: 'CANCEL', event: B.OrderCanceledEvent }
+| { type: 'EXPIRE', event: B.OrderExpiredEvent }
+
+type State =
+  | { value: 'uninitialized', context: B.OrderCreatedEvent }
+  | { value: 'created', context: B.OrderCreatedEvent }
+  | { value: 'accepted', context: B.OrderAcceptedEvent }
+  | { value: 'rejected', context: B.OrderRejectedEvent }
+  | { value: 'filled', context: B.OrderFilledEvent }
+  | { value: 'closed', context: B.OrderClosedEvent }
+  | { value: 'canceled', context: B.OrderCanceledEvent }
+  | { value: 'expired', context: B.OrderExpiredEvent }
+
 class LocalOrderStream<Props extends B.OrderProps> extends B.DebugOrderStream<Props> {
-    private canBeClosed: boolean = false;
-    private canBeCanceled: boolean = true;
+    private service: StateMachine.Service<Context, Event, State>;
     constructor(props: Props) {
         super(props);
-        const reset = () => {
-            this.canBeClosed = false;
-            this.canBeCanceled = false;
-        }
-        this.on("accepted", () => {
-            this.canBeClosed = false;
-            this.canBeCanceled = true;
-        })
-        this.on("rejected", () => reset())
-        this.on("filled", () => {
-            this.canBeClosed = true;
-            this.canBeCanceled = false;
-        })
-        this.on("closed", () => reset())
-        this.on("canceled", () => reset())
-        this.on("ended", () => reset())
+        const machine = createMachine<Context, Event, State>({
+            initial: "uninitialized",
+            states: {
+                uninitialized: {
+                    on: {
+                        CREATE: 'created'
+                    }
+                },
+                created: {
+                    entry: (_context, event) => {
+                        if(event.type === "CREATE") {
+                            this.emitCreated(event.event)
+                        }
+                    },
+                    on: {
+                        ACCEPT: 'accepted',
+                        REJECT: 'rejected',
+                        CANCEL: 'canceled'
+                    }
+                },
+                accepted: {
+                    entry: (_context, event) => {
+                        if(event.type === "ACCEPT") {
+                            this.emitAccepted(event.event)
+                        }
+                    },
+                    on: {
+                        FILL: 'filled',
+                        CANCEL: 'canceled',
+                        EXPIRE: 'expired'
+                    }
+                },
+                filled: {
+                    entry: (_context, event) => {
+                        if(event.type === "FILL") {
+                            this.emitFilled(event.event)
+                        }
+                    },
+                    on: {
+                        CLOSE: 'closed'
+                    }
+                },
+                rejected: {
+                    entry: (_context, event) => {
+                        if(event.type === "REJECT") {
+                            this.emitRejected(event.event)
+                            this.emitEnded(event.event)
+                        }
+                    }
+                },
+                closed: {  
+                    entry: (_context, event) => {
+                        if(event.type === "CLOSE") {
+                            this.emitClosed(event.event)
+                            this.emitEnded(event.event)
+                        }
+                    }
+                },
+                canceled: { 
+                    entry: (_context, event) => {
+                        if(event.type === "CANCEL") {
+                            this.emitCanceled(event.event)
+                            this.emitEnded(event.event)
+                        }
+                    }
+                },
+                expired: {
+                    entry: (_context, event) => {
+                        if(event.type === "EXPIRE") {
+                            this.emitExpired(event.event)
+                            this.emitEnded(event.event)
+                        }
+                    }
+                },
+            }
+        });
+        this.service = interpret(machine);
+        this.service.start();
     }
     async close(): Promise<B.OrderClosedEvent> {
-        if(this.canBeClosed) {
-            const {timestamp, price: exit, profitLoss} = await this.profitLoss()
-            this.emitClosed({ timestamp, exit, profitLoss})
-            this.emitEnded({ timestamp, exit, profitLoss})
-            return { timestamp, exit, profitLoss}
+        const {timestamp, price: exit, profitLoss} = await this.profitLoss();
+        this.tryClose({ timestamp, exit, profitLoss })
+        if(this.service.state.matches("closed")) {
+            return this.closed()
         }
-        throw new Error(`order ${this.props.id} cannot be closed ${JSON.stringify({canBeClosed: this.canBeClosed, canBeCanceled: this.canBeCanceled})}`);
+        throw new Error(`order ${this.props.id} cannot be closed (${JSON.stringify(this.service.state)})`);
     }
     async cancel(): Promise<B.OrderCanceledEvent> {
-        if(this.canBeCanceled) {
-            this.emitCanceled({timestamp: Date.now()})
-            this.emitEnded({timestamp: Date.now()})
-            return { timestamp: Date.now() }
+        const timestamp = Date.now();
+        this.tryCancel({ timestamp })
+        if(this.service.state.matches("canceled")) {
+            return this.canceled();
         }
-        throw new Error(`order ${this.props.id} cannot be canceled ${JSON.stringify({canBeClosed: this.canBeClosed, canBeCanceled: this.canBeCanceled})}`);
+        throw new Error(`order ${this.props.id} cannot be canceled (${JSON.stringify(this.service.state)})`);
     }
     async end(): Promise<B.OrderEndedEvent> {
-        if(this.canBeCanceled) {
-            await this.cancel();
-        } else if(this.canBeClosed) {
-            await this.close();
+        if(["created", "accepted", "canceled"].includes(this.service.state.value)) {
+            return this.cancel();
+        } else if(["filled", "closed"].includes(this.service.state.value)) {
+            return this.close();
         }
         return this.ended()
     }
+
+    
+    tryCreate(e: B.OrderCreatedEvent): void {
+        this.service.send({ type: "CREATE", event: e})
+      }
+    
+      tryAccept(e: B.OrderAcceptedEvent): void {
+          this.service.send({ type: "ACCEPT", event: e})
+      }
+    
+      tryReject(e: B.OrderRejectedEvent): void {
+          this.service.send({ type: "REJECT", event: e})
+      }
+    
+      tryFill(e: B.OrderFilledEvent): void {
+          this.service.send({ type: "FILL", event: e})
+      }
+    
+      tryClose(e: B.OrderClosedEvent): void {
+          this.service.send({ type: "CLOSE", event: e})
+      }
+    
+      tryCancel(e: B.OrderCanceledEvent): void {
+          this.service.send({ type: "CANCEL", event: e})
+      }
+    
+      tryExpire(e: B.OrderExpiredEvent): void {
+          this.service.send({ type: "EXPIRE", event: e})
+      }
 }
 
 async function fromSpotPrices<Props extends B.OrderProps>(props: Props, spots: B.SpotPricesStream, buyCond: (e: B.AskPriceChangedEvent) => boolean, sellCond: (e: B.BidPriceChangedEvent) => boolean): Promise<B.OrderStream<Props>> {
     const stream = new LocalOrderStream<Props>(props);
-    stream.emitCreated({ timestamp: Date.now() })
+    stream.tryCreate({ timestamp: Date.now() })
     if (props.tradeSide === "BUY") {
         const fill = (e: B.AskPriceChangedEvent): boolean => {
             const { timestamp, ask: entry } = e;
             if (buyCond(e)) {
-                stream.emitFilled({ timestamp, entry })
+                stream.tryFill({ timestamp, entry })
 
                 const update = (e: B.BidPriceChangedEvent) => {
                     const {timestamp, bid: price} = e
@@ -65,8 +183,7 @@ async function fromSpotPrices<Props extends B.OrderProps>(props: Props, spots: B
 
                     if(props.stopLoss && props.stopLoss >= price ||
                         props.takeProfit && props.takeProfit <= price) {
-                        stream.emitClosed({timestamp, exit: price, profitLoss})
-                        stream.emitEnded({timestamp, exit: price, profitLoss})
+                        stream.tryClose({timestamp, exit: price, profitLoss})
                     }
                 }
                 spots.bid().then(e => {
@@ -89,7 +206,7 @@ async function fromSpotPrices<Props extends B.OrderProps>(props: Props, spots: B
         const fill = (e: B.BidPriceChangedEvent): boolean => {
             const { timestamp, bid: entry } = e;
             if (sellCond(e)) {
-                stream.emitFilled({ timestamp, entry })
+                stream.tryFill({ timestamp, entry })
 
                 const update = (e: B.AskPriceChangedEvent) => {
                     const {timestamp, ask: price} = e
@@ -98,8 +215,7 @@ async function fromSpotPrices<Props extends B.OrderProps>(props: Props, spots: B
 
                     if(props.stopLoss && props.stopLoss <= price ||
                         props.takeProfit && props.takeProfit >= price) {
-                        stream.emitClosed({timestamp, exit: price, profitLoss})
-                        stream.emitEnded({timestamp, exit: price, profitLoss})
+                        stream.tryClose({timestamp, exit: price, profitLoss})
                     }
                 }
                 spots.ask().then(e => {
@@ -123,8 +239,7 @@ async function fromSpotPrices<Props extends B.OrderProps>(props: Props, spots: B
         const expiration = (e: B.AskPriceChangedEvent | B.BidPriceChangedEvent) => {
             const { timestamp } = e;
             if(timestamp >= props.expiresAt!) {
-                stream.emitExpired({ timestamp })
-                stream.emitEnded({ timestamp })
+                stream.tryExpire({ timestamp })
             }
         }
         stream.once("accepted", () => {
@@ -140,7 +255,7 @@ async function fromSpotPrices<Props extends B.OrderProps>(props: Props, spots: B
             });
         })
     }
-    stream.emitAccepted({ timestamp: Date.now() })
+    stream.tryAccept({ timestamp: Date.now() })
     return stream;
 }
 
