e9a1ec077bac34be21ce2901fd82c6fc106536d0
Claas Ahlrichs <claasahl@gmail.com>
2020-05-18 18:16:06 +0200

    fixed lifecycle tests

--- src/services/base/spotPrices.test.js
+++ src/services/base/spotPrices.test.js
@@ -137,38 +137,44 @@ describe("DebugSpotPricesStream", () => {
         })
     })
 
-    describe("emitXXX helpers", () => {
+    describe("lifecylce", () => {
         test("should emit 'ask' event", done => {
             const props = { symbol: Symbol.for("abc"), a: 2 }
             const stream = new DebugSpotPricesStream(props)
             const event = { ask: 23, timestamp: 123 };
-            stream.once("ask", e => {
-                expect(e).toBe(event);
-                done()
+            stream.once("data", e => {
+                if(e.type === "ASK_PRICE_CHANGED") {
+                    expect(e).toStrictEqual({...event, type: "ASK_PRICE_CHANGED"});
+                    done();
+                }
             })
-            stream.emitAsk(event)
+            stream.tryAsk(event)
         })
 
         test("should emit 'bid' event", done => {
             const props = { symbol: Symbol.for("abc"), a: 2 }
             const stream = new DebugSpotPricesStream(props)
             const event = { bid: 23, timestamp: 123 };
-            stream.once("bid", e => {
-                expect(e).toBe(event);
-                done()
+            stream.once("data", e => {
+                if(e.type === "BID_PRICE_CHANGED") {
+                    expect(e).toStrictEqual({...event, type: "BID_PRICE_CHANGED"});
+                    done();
+                }
             })
-            stream.emitBid(event)
+            stream.tryBid(event)
         })
     
         test("should emit 'price' event", done => {
             const props = { symbol: Symbol.for("abc"), a: 2 }
             const stream = new DebugSpotPricesStream(props)
             const event = { ask: 22, bid: 23, timestamp: 123 };
-            stream.once("price", e => {
-                expect(e).toBe(event);
-                done()
+            stream.on("data", e => {
+                if(e.type === "PRICE_CHANGED") {
+                    expect(e).toStrictEqual({...event, type: "PRICE_CHANGED"});
+                    done();
+                }
             })
-            stream.emitPrice(event)
+            stream.tryPrice(event)
         })
     })
 })
