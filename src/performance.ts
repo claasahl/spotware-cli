3c9781a436861ce92a0be5d67226db26b8ea0083
Claas Ahlrichs <claasahl@gmail.com>
2020-05-25 17:19:53 +0200

    updated performance test results

--- src/performance.ts
+++ src/performance.ts
@@ -2,9 +2,9 @@ import { fromLogFiles } from "./services/local";
 import ms from "ms";
 
 function main() {
-  // about 25s (with DEBUG="*")
-  // about 7s (without DEBUG="*")
-  // about 7s (with DEBUG="account:*,order:*")
+  // about 18s (with DEBUG="*")
+  // about 4s (without DEBUG="*")
+  // about 4s (with DEBUG="account:*,order:*")
   const time = Date.now();
   process.on("exit", () => console.log(ms(Date.now() - time)))
 
@@ -21,24 +21,3 @@ function main() {
   spots.on("data", () => { })
 }
 main();
-
-// async function main2() {
-//   // about 3s
-//   const time = Date.now();
-//   process.on("exit", () => console.log(ms(Date.now() - time)))
-
-//   const iterator = fr0mLogFiles([
-//     "./store/2020-04-27.log",
-//     "./store/2020-04-28.log",
-//     "./store/2020-04-29.log",
-//     "./store/2020-04-30.log",
-//     "./store/2020-05-01.log",
-//   ])
-//   let count = 0
-//   for await (const a of iterator) {
-//     a
-//     setImmediate(() => count++)
-//   }
-//   console.log(count)
-// }
-// main2();
