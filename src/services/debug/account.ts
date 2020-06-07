d33f3e50776380a2d1388ddbdb408a27c50a22d7
Claas Ahlrichs <claasahl@gmail.com>
2020-06-07 17:03:54 +0200

    drafted debug impl. of services

---
+++ src/services/debug/account.ts
@@ -0,0 +1,40 @@
+import {PassThrough} from "stream";
+import debug from "debug";
+
+import * as B from "../base";
+
+const events: B.AccountEvent["type"][] = ["BALANCE_CHANGED" , "TRANSACTION" , "EQUITY_CHANGED" , "CREATED" , "ACCEPTED" , "REJECTED" , "EXPIRED" , "CANCELED" , "FILLED" , "PROFITLOSS" , "CLOSED" , "ENDED"]
+
+export class AccountStream extends PassThrough implements B.AccountStream {
+  readonly props: B.AccountProps;
+  private readonly log: debug.Debugger;
+
+  constructor(props: B.AccountProps) {
+    super({objectMode: true});
+    this.props = Object.freeze(props);
+    this.log = debug("account");
+  }
+
+  push(chunk: B.AccountEvent | null, encoding?: BufferEncoding): boolean {
+    if(chunk && events.includes(chunk.type)) {
+      this.log("%j", event);
+    }
+    return super.push(chunk, encoding);
+  }
+
+  marketOrder(_props: B.AccountSimpleMarketOrderProps): B.OrderStream<B.MarketOrderProps> {
+    throw new Error("Method not implemented.");
+  }
+
+  stopOrder(_props: B.AccountSimpleStopOrderProps): B.OrderStream<B.StopOrderProps> {
+    throw new Error("Method not implemented.");
+  }
+
+  spotPrices(_props: B.AccountSimpleSpotPricesProps): B.SpotPricesStream {
+    throw new Error("Method not implemented.");
+  }
+
+  trendbars(_props: B.AccountSimpleTrendbarsProps): B.TrendbarsStream {
+    throw new Error("Method not implemented.");
+  }
+}
---
+++ src/services/debug/order.ts
@@ -0,0 +1,38 @@
+import {PassThrough} from "stream";
+import debug from "debug";
+
+import * as B from "../base";
+
+const events: B.OrderEvent["type"][] = ["CREATED" , "ACCEPTED" , "REJECTED" , "EXPIRED" , "CANCELED" , "FILLED" , "PROFITLOSS" , "CLOSED" , "ENDED"]
+
+export class OrderStream<Props extends B.OrderProps> extends PassThrough implements B.OrderStream<Props> {
+  readonly props: Props;
+  private readonly log: debug.Debugger;
+
+  constructor(props: Props) {
+    super({objectMode: true});
+    this.props = Object.freeze(props);
+    this.log = debug("order")
+      .extend(props.symbol.toString())
+      .extend(props.id);
+  }
+
+  push(chunk: B.OrderEvent | null, encoding?: BufferEncoding): boolean {
+    if(chunk && events.includes(chunk.type)) {
+      this.log("%j", event);
+    }
+    return super.push(chunk, encoding);
+  }
+
+  closeOrder(): void {
+    throw new Error("Method not implemented.");
+  }
+
+  cancelOrder(): void {
+    throw new Error("Method not implemented.");
+  }
+
+  endOrder(): void {
+    throw new Error("Method not implemented.");
+  }
+}
