d73b529dbcfc4b17e0b2403a64fc7eec69e9dec0
Claas Ahlrichs <claasahl@gmail.com>
2020-06-11 19:35:27 +0200

    formatted sources

--- src/services/generic/trendbars.ts
+++ src/services/generic/trendbars.ts
@@ -22,13 +22,13 @@ function accumulateTrendbar(
 ): T.TrendbarEvent {
   const next = { ...prev };
   if (index === 0) {
-      next.open = curr.bid;
+    next.open = curr.bid;
   }
   if (prev.high < curr.bid) {
-      next.high = curr.bid;
+    next.high = curr.bid;
   }
   if (prev.low > curr.bid) {
-      next.low = curr.bid;
+    next.low = curr.bid;
   }
   next.close = curr.bid;
   return next;
@@ -39,13 +39,13 @@ function toTrendbar(
   events: T.BidPriceChangedEvent[]
 ): T.TrendbarEvent {
   const seed: T.TrendbarEvent = {
-      type: "TRENDBAR",
-      open: 0,
-      high: Number.MIN_VALUE,
-      low: Number.MAX_VALUE,
-      close: 0,
-      timestamp,
-      volume: 0
+    type: "TRENDBAR",
+    open: 0,
+    high: Number.MIN_VALUE,
+    low: Number.MAX_VALUE,
+    close: 0,
+    timestamp,
+    volume: 0
   };
   return events.reduce(accumulateTrendbar, seed);
 }
@@ -55,9 +55,9 @@ export class ToTrendbars extends Transform implements T.TrendbarsStream {
   private readonly values: Array<T.BidPriceChangedEvent> = [];
 
   constructor(props: T.TrendbarsProps) {
-      super({ objectMode: true });
-      this.props = Object.freeze(props);
-      L.logTrendbarEvents(this);
+    super({ objectMode: true });
+    this.props = Object.freeze(props);
+    L.logTrendbarEvents(this);
   }
 
   push(event: T.TrendbarEvent | null): boolean {
@@ -65,46 +65,46 @@ export class ToTrendbars extends Transform implements T.TrendbarsStream {
   }
 
   _transform(chunk: T.SpotPricesEvent, _encoding: string, callback: TransformCallback): void {
-      if (chunk.type === "BID_PRICE_CHANGED") {
-          this.values.push(chunk);
-          const bucket1 = this.bucket(this.values[0]);
-          const bucket2 = this.bucket(this.values[this.values.length - 1]);
-          if (bucket1.begin !== bucket2.begin) {
-              const eventsInBucket = this.values.filter(
-                  e => this.bucket(e).begin === bucket1.begin
-              );
-              this.values.splice(0, eventsInBucket.length);
-              this.push(toTrendbar(bucket1.begin, eventsInBucket))
-          }
-      } else if (chunk.type === "ASK_PRICE_CHANGED") {
-          if (this.values.length === 0) {
-              return callback();
-          }
-          const bucket1 = this.bucket(this.values[0]);
-          const bucket2 = this.bucket(chunk);
-          if (bucket1.begin !== bucket2.begin) {
-              const eventsInBucket = this.values.filter(
-                  e => this.bucket(e).begin === bucket1.begin
-              );
-              this.values.splice(0, eventsInBucket.length);
-              this.push(toTrendbar(bucket1.begin, eventsInBucket))
-          }
-        }
+    if (chunk.type === "BID_PRICE_CHANGED") {
+      this.values.push(chunk);
+      const bucket1 = this.bucket(this.values[0]);
+      const bucket2 = this.bucket(this.values[this.values.length - 1]);
+      if (bucket1.begin !== bucket2.begin) {
+        const eventsInBucket = this.values.filter(
+          e => this.bucket(e).begin === bucket1.begin
+        );
+        this.values.splice(0, eventsInBucket.length);
+        this.push(toTrendbar(bucket1.begin, eventsInBucket))
+      }
+    } else if (chunk.type === "ASK_PRICE_CHANGED") {
+      if (this.values.length === 0) {
         return callback();
+      }
+      const bucket1 = this.bucket(this.values[0]);
+      const bucket2 = this.bucket(chunk);
+      if (bucket1.begin !== bucket2.begin) {
+        const eventsInBucket = this.values.filter(
+          e => this.bucket(e).begin === bucket1.begin
+        );
+        this.values.splice(0, eventsInBucket.length);
+        this.push(toTrendbar(bucket1.begin, eventsInBucket))
+      }
+    }
+    return callback();
   }
 
   bucket(e: T.SpotPricesEvent): Bucket {
-      const { timestamp } = e;
-      const { period } = this.props
-      return bucket(timestamp, period)
+    const { timestamp } = e;
+    const { period } = this.props
+    return bucket(timestamp, period)
   }
 }
 
 export function toTrendbars(props: T.TrendbarsProps & { spots: T.SpotPricesStream }): T.TrendbarsStream {
   const { spots, ...originalProps } = props;
   return pipeline(
-      spots,
-      new ToTrendbars(originalProps),
-      err => console.log("pipeline callback", err)
+    spots,
+    new ToTrendbars(originalProps),
+    err => console.log("pipeline callback", err)
   )
 }
