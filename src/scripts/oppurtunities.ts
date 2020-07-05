c8eb48957c3cd4e28dd1f229b709d554164137b7
Claas Ahlrichs <claasahl@gmail.com>
2020-07-05 12:25:56 +0200

    split scripts into dedicate files

--- src/scripts/oppurtunities.ts
+++ src/scripts/oppurtunities.ts
@@ -1,116 +1,14 @@
-import { insideBarMomentumStrategy } from "../services/insideBarMomentumStrategy";
-import { fromNothing, fromFiles } from "../services/local";
-import config from "../config";
-import { fromSomething } from "../services/spotware/account";
+import { fromFiles } from "../services/local";
 import ms from "ms";
 import fs from "fs"
 import path from "path"
-import readline from "readline";
-import { obj as multistream } from "multistream";
 import { finished } from "stream";
 import { AskPriceChangedEvent, BidPriceChangedEvent } from "../services/types";
 import * as G from "../services/generic"
 import * as T from "../services/types"
 import { bullish, bearish } from "indicators"
 
-function local(inputs: string[]) {
-    const name = "BTC/EUR";
-    const symbol = Symbol.for(name);
-    const currency = Symbol.for("EUR");
-    console.log(inputs);
-    const spots = () => fromFiles({
-        paths: inputs,
-        symbol
-    });
-    const account = fromNothing({ currency, spots, initialBalance: 1000 })
-    const trendbars = account.trendbars({ symbol, period: ms("15min") })
-    trendbars.on("data", e => {
-        if(e.timestamp === 1593110700000) {
-            account.limitOrder({ id: "1", symbol, tradeSide: "BUY", volume: 0.01, enter: 8244.84, takeProfit: 8254.66, stopLoss: 8232.99}).resume();
-        }
-    })
-    setImmediate(() => {
-        // account.stopOrder({ id: "1", symbol, tradeSide: "SELL", volume: 1, enter: 6613});
-        // account.marketOrder({ id: "1", symbol, tradeSide: "BUY", volume: 1, takeProfit: 6614.0});
-        // account.marketOrder({ id: "1", symbol, tradeSide: "SELL", volume: 1, stopLoss: 6613.0 });
-    });
-    account.resume(); // consume account events
-}
-
-async function spotware() {
-    const currency = Symbol.for("EUR");
-    const symbol = Symbol.for("BTC/EUR");
-    const account = fromSomething({ ...config, currency })
-    account.trendbars({ symbol, period: 10000 }).on("data", console.log)
-    setTimeout(async () => {
-        const stream = account.limitOrder({ id: "1", symbol, tradeSide: "BUY", volume: 0.01, enter: 17000, stopLoss: 16900, takeProfit: 17100, expiresAt: Date.now() + 10000 })
-        stream.on("data", e => console.log("---", e));
-        stream.on("end", () => console.log("--- END",));
-        stream.on("close", () => console.log("--- CLOSE",));
-        stream.on("error", err => console.log("--- ERROR", err));
-        // setInterval(() => stream.endOrder(), 5000)
-    }, 5000)
-}
-
-function insideBarLocal(inputs: string[]) {
-    const currency = Symbol.for("EUR");
-    const initialBalance = 177.59;
-    const period = ms("15min");
-    const symbol = Symbol.for("BTC/EUR");
-    const enterOffset = 0.1;
-    const stopLossOffset = 0.4;
-    const takeProfitOffset = 0.8;
-    const minTrendbarRange = 15;
-    const volume = 0.01;
-    const expiresIn = ms("30min")
-    const spots = () => fromFiles({
-        paths: inputs,
-        symbol
-    });
-    const account = fromNothing({ currency, initialBalance, spots })
-    insideBarMomentumStrategy({ account, period, symbol, enterOffset, stopLossOffset, takeProfitOffset, minTrendbarRange, volume, expiresIn })
-    account.resume(); // consume account events
-}
-
-async function review(output: string, inputs: string[]) {
-    const fileStream = multistream(inputs.map(file => fs.createReadStream(file)))
-    const rl = readline.createInterface({
-        input: fileStream,
-        crlfDelay: Infinity
-    });
-    const orders = new Map<string, any>()
-    for await (const line of rl) {
-        if (line.indexOf("account") >= 0) {
-            const logEntry = /({.*})/.exec(line);
-            const data = JSON.parse(logEntry![1])
-            if ("id" in data) {
-                const id = data.id;
-                if (!orders.has(id)) {
-                    orders.set(id, {})
-                }
-                const order = orders.get(id)!
-                const fromTimestamp = Math.min(data.timestamp, order.fromTimestamp || Number.MAX_VALUE)
-                const toTimestamp = Math.max(data.timestamp, order.toTimestamp || Number.MIN_VALUE)
-                const from = new Date(fromTimestamp)
-                const to = new Date(toTimestamp)
-                const duration = ms(toTimestamp - fromTimestamp)
-
-                delete data.type;
-                delete data.timestamp;
-                Object.assign(order, data, { fromTimestamp, toTimestamp, from, to, duration })
-            }
-        }
-    }
-
-    console.log(orders)
-    const out = fs.createWriteStream(output)
-    out.write(`id;type;tradeSide;volume;enter;stopLoss;takeProfit;fromTimestamp;from;toTimestamp;to;expiresTimestamp;expires;duration;entry;exit;price;profitLoss;modified stopLoss;modified takeProfit\n`)
-    for (const [_id, data] of orders) {
-        out.write(`${data.id};${data.orderType};${data.tradeSide};${data.volume};${data.enter};${data.stopLoss};${data.takeProfit};${data.fromTimestamp};${data.from.toISOString()};${data.toTimestamp};${data.to.toISOString()};${data.expiresAt};${new Date(data.expiresAt).toISOString()};${data.duration};${data.entry || ""};${data.exit || ""};${data.price || ""};${data.profitLoss || ""};${data.modifiedStopLoss || ""};${data.modifiedTakeProfit || ""}\n`)
-    }
-    out.end();
-}
-async function temp(output: string, inputs: string[]) {
+export default async function main(output: string, inputs: string[]) {
     type Oppurtunity = {
         orderType: "BUY",
         enter: AskPriceChangedEvent,
@@ -290,27 +188,4 @@ async function temp(output: string, inputs: string[]) {
         outCleaned.write(JSON.stringify(cleaned, null, 2))
         outCleaned.end();
     })
-}
-
-if (process.argv[2] === "local") {
-    const inputs = process.argv.slice(3)
-    local(inputs);
-} else if (process.argv[2] === "spotware") {
-    spotware();
-} else if (process.argv[2] === "insidebar") {
-    const inputs = process.argv.slice(3)
-    insideBarLocal(inputs);
-} else if (process.argv[2] === "temp") {
-    const output = process.argv[3];
-    const inputs = process.argv.slice(4)
-    temp(output, inputs);
-} else if (process.argv[2] === "review") {
-    const output = process.argv[3];
-    const inputs = process.argv.slice(4)
-    review(output, inputs);
-} else {
-    console.log("npm run script local \"input1.log\" \"input2.log\"")
-    console.log("npm run script spotware")
-    console.log("npm run script insidebar \"input1.log\" \"input2.log\"")
-    console.log("npm run script review \"output.csv\" \"input1.log\" \"input2.log\"")
 }
