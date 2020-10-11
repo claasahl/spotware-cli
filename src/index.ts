3a70782bd2801704b3774cd0943cb35b61af8f7b
Claas Ahlrichs <claasahl@gmail.com>
2020-10-11 12:48:12 +0200

    imported sample code

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,27 @@
-// happy coding 👻
-console.log("hello world");
+import tls from "tls";
+import {
+  SpotwareClientSocket,
+  ProtoOAPayloadType,
+  FACTORY,
+} from "@claasahl/spotware-adapter";
+
+const host = "demo.ctraderapi.com";
+const port = 5035;
+
+const socket = tls.connect(port, host);
+socket.once("secureConnect", () => {
+  console.log(`requesting version number from ${host}:${port}`);
+  s.write(FACTORY.PROTO_OA_VERSION_REQ());
+});
+
+const s = new SpotwareClientSocket(socket);
+s.on("data", (message) => {
+  switch (message.payloadType) {
+    case ProtoOAPayloadType.PROTO_OA_VERSION_RES:
+      console.log(`got response from ${host}:${port}`);
+      console.log(`server version: ${message.payload.version}`);
+      s.end();
+      break;
+    // ...
+  }
+});
