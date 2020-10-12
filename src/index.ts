cd9d44440ab451b3b64f90605fa2ff588f5989c0
Claas Ahlrichs <claasahl@gmail.com>
2020-10-12 18:40:39 +0200

    imported example

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,54 @@
-import tls from "tls";
-import {
-  SpotwareClientSocket,
-  ProtoOAPayloadType,
-  FACTORY,
-} from "@claasahl/spotware-adapter";
+import { connect as tlsConnect } from "tls";
+import { connect as netConnect } from "net";
+import { SpotwareClientSocket, FACTORY } from "@claasahl/spotware-adapter";
+import Accounts from "./accounts";
+import { Events } from "./events";
+import Spots from "./spots";
+import Symbols from "./symbols";
 
-const host = "demo.ctraderapi.com";
-const port = 5035;
+const config = {
+  host: process.env.SPOTWARE__HOST || "live.ctraderapi.com",
+  port: Number(process.env.SPOTWARE__PORT) || 5035,
+  clientId: process.env.SPOTWARE__CLIENT_ID || "",
+  clientSecret: process.env.SPOTWARE__CLIENT_SECRET || "",
+  accessToken: process.env.access_token || "",
+};
 
-const socket = tls.connect(port, host);
-socket.once("secureConnect", () => {
-  console.log(`requesting version number from ${host}:${port}`);
-  s.write(FACTORY.PROTO_OA_VERSION_REQ());
+const events = new Events();
+const isLocalhost = config.host === "localhost";
+const socket = isLocalhost
+  ? netConnect(config.port, config.host)
+  : tlsConnect(config.port, config.host);
+const event = isLocalhost ? "connect" : "secureConnect";
+const s = new SpotwareClientSocket(socket);
+socket.once(event, () => s.write(FACTORY.PROTO_OA_VERSION_REQ()));
+
+const accounts = new Accounts(s, config, events);
+s.on("data", (msg) => accounts.onMessage(msg));
+socket.once(event, () => accounts.onInit());
+
+events.on("account", (account) => {
+  if (!account.authenticated || !account.depositAssetId) {
+    return;
+  }
+  const symbols = new Symbols(s, account.ctidTraderAccountId, events);
+  s.on("data", (msg) => symbols.onMessage(msg));
+  symbols.onInit();
 });
 
-const s = new SpotwareClientSocket(socket);
-s.on("data", (message) => {
-  switch (message.payloadType) {
-    case ProtoOAPayloadType.PROTO_OA_VERSION_RES:
-      console.log(`got response from ${host}:${port}`);
-      console.log(`server version: ${message.payload.version}`);
-      s.end();
-      break;
-    // ...
+const ASSET_CLASSES = ["Forex", "Metals", "Crypto Currency"];
+events.on("symbol", (symbol) => {
+  if (!ASSET_CLASSES.includes(symbol.assetClass)) {
+    return;
   }
+  console.log(symbol.symbolId, symbol.symbolName);
+  if (symbol.symbolName === "BTC/EUR") {
+    const spots = new Spots(s, symbol, events);
+    s.on("data", (msg) => spots.onMessage(msg));
+    spots.onInit();
+  }
+});
+
+events.on("spot", (spot) => {
+  console.log(spot);
 });
