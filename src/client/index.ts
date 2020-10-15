4ac2271cce890316be3e776af6e81d5d2dd951e6
Claas Ahlrichs <claasahl@gmail.com>
2020-10-15 19:59:44 +0200

    added notes

--- src/client/index.ts
+++ src/client/index.ts
@@ -55,3 +55,16 @@ events.on("symbol", async (symbol) => {
 events.on("spot", (spot) => {
   console.log(spot);
 });
+
+// "break out" separate stream that focuses on (highlevel) application (ideally both read and write)
+// "break out" separate streams that focus on individual accounts (ideally both read and write)
+// "break out" separate streams that focus on spot prices for a symbol (ideally both read and write)
+
+// was not able to use Duplex streams for spot prices stream.
+// -- calls to socket.read() would "eat" data away from other consumers
+// -- only flowing mode would be doable, but then backpressure would not be supported (other than forcefully pausing the stream -- which might be exactlz how nodejs implements it)
+
+// -----
+
+// prepare list of relevant messages for each of the above streams
+// ... alternatively, consider wrapping spotware stream in another Duplex stream which transforms spotware events into a more digestable format
