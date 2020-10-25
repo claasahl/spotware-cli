8479c1a5893f47f624b943f738f3a52f81678b02
Claas Ahlrichs <claasahl@gmail.com>
2020-10-25 17:04:12 +0100

    render candle

--- src/data/svg.ts
+++ src/data/svg.ts
@@ -1,7 +1,7 @@
 import { JSDOM } from "jsdom";
 import * as d3 from "d3";
 import fs from "fs";
-import { bullish } from "indicators";
+import { bullish, upper, lower } from "indicators";
 
 import { Trendbar } from "../utils";
 import { ProtoOATrendbarPeriod } from "@claasahl/spotware-adapter";
@@ -17,27 +17,34 @@ function bar(
   trendbar: Trendbar
 ) {
   const color = bullish(trendbar) ? BULLISH_COLOR : BEARISH_COLOR;
+  const price = d3
+    .scaleLinear()
+    .domain([trendbar.low, trendbar.high])
+    .rangeRound([105, 5]);
   const x = 110;
   svg
     .append("rect")
     .attr("x", x - BAR_WIDTH_HALF)
-    .attr("y", trendbar.open)
+    .attr("y", price(upper(trendbar)) || -100)
     .attr("width", BAR_WIDTH_FULL)
-    .attr("height", trendbar.close)
-    .attr("style", `fill: ${color}`);
+    .attr(
+      "height",
+      (price(upper(trendbar)) || -100) - (price(lower(trendbar)) || -100)
+    )
+    .attr("style", `fill: ${color}; stroke: ${color}; stroke-width: 2`);
   svg
     .append("line")
     .attr("x1", x)
-    .attr("y1", trendbar.high)
+    .attr("y1", price(trendbar.high) || -100)
     .attr("x2", x)
-    .attr("y2", trendbar.low)
+    .attr("y2", price(trendbar.low) || -100)
     .attr("style", `stroke: ${color}; stroke-width: 2`);
 }
 
 const dom = new JSDOM(`<!DOCTYPE html><body></body>`);
 
-const body = d3.select(dom.window.document.querySelector("body"));
-const svg = body
+const htmlBody = d3.select(dom.window.document.querySelector("body"));
+const svg = htmlBody
   .append("svg")
   .attr("width", 1000)
   .attr("height", 1000)
@@ -54,11 +61,11 @@ const trendbar: Trendbar = {
   open: 10,
   high: 40,
   low: 5,
-  close: 9,
+  close: 10,
   volume: 1,
   period: ProtoOATrendbarPeriod.D1,
   timestamp: Date.now(),
 };
 bar(svg, trendbar);
 
-fs.writeFileSync("out.svg", body.html());
+fs.writeFileSync("out.svg", htmlBody.html());
