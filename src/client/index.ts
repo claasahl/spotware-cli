b965d817e7a7de2b8b5a5ec425de0793a5ff2471
Claas Ahlrichs <claasahl@gmail.com>
2020-10-25 09:49:29 +0100

    moved useTLS

--- src/client/index.ts
+++ src/client/index.ts
@@ -14,9 +14,9 @@ import { Events } from "./events";
 const log = debug("custom-client");
 const host = process.env.host || "live.ctraderapi.com";
 const port = Number(process.env.port) || 5035;
+const useTLS = process.env.useTLS === "true";
 
 const events = new Events();
-const useTLS = process.env.useTLS === "true";
 const socket = useTLS ? tlsConnect(port, host) : netConnect(port, host);
 const event = useTLS ? "secureConnect" : "connect";
 const s = new SpotwareClientSocket(socket);
