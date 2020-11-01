749f3afbce033d297ae77a47009ab53b45a13a47
Claas Ahlrichs <claasahl@gmail.com>
2020-11-01 14:12:38 +0100

    updated list of max intervals

--- src/data/trendbars.ts
+++ src/data/trendbars.ts
@@ -11,6 +11,7 @@ function interval(period: ProtoOATrendbarPeriod): number {
     case ProtoOATrendbarPeriod.H1:
       return 21168000000;
     case ProtoOATrendbarPeriod.M5:
+    case ProtoOATrendbarPeriod.M1:
       return 3024000000;
     default:
       return 99999999999;
