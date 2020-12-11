0171c817ae7d0aac882e763e8b7ea6f111dc1184
Claas Ahlrichs <claasahl@gmail.com>
2020-12-11 18:49:42 +0100

    more tests

--- src/utils/trendbars.test.js
+++ src/utils/trendbars.test.js
@@ -24,3 +24,28 @@ describe("isLiveTrendbar", () => {
     expect(result).toBe(true);
   });
 });
+
+describe("isTrendbar", () => {
+  test("postive example", () => {
+    const result = T.isTrendbar({
+      volume: 8,
+      low: 976306000,
+      deltaOpen: 727000,
+      deltaClose: 176000,
+      deltaHigh: 971000,
+      utcTimestampInMinutes: 26716810,
+    });
+    expect(result).toBe(true);
+  });
+  test("negative example", () => {
+    const result = T.isTrendbar({
+      volume: 10,
+      period: 1,
+      low: 976891000,
+      deltaOpen: 0,
+      deltaHigh: 18000,
+      utcTimestampInMinutes: 26717050,
+    });
+    expect(result).toBe(false);
+  });
+});
