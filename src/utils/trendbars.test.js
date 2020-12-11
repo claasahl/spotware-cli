9ff9f1d80ffa1a09af5e1e6c37848b8e2cf90d57
Claas Ahlrichs <claasahl@gmail.com>
2020-12-11 18:58:52 +0100

    more tests

--- src/utils/trendbars.test.js
+++ src/utils/trendbars.test.js
@@ -49,3 +49,50 @@ describe("isTrendbar", () => {
     expect(result).toBe(false);
   });
 });
+
+describe("toTrendbar", () => {
+  test("convert historical trendbar", () => {
+    const result = T.toTrendbar(
+      {
+        volume: 8,
+        low: 976306000,
+        deltaOpen: 727000,
+        deltaClose: 176000,
+        deltaHigh: 971000,
+        utcTimestampInMinutes: 26716810,
+      },
+      2
+    );
+    expect(result).toStrictEqual({
+      timestamp: 26716810 * 60000,
+      period: 2,
+      open: 977033000,
+      high: 977277000,
+      low: 976306000,
+      close: 976482000,
+      volume: 8,
+    });
+  });
+  test("convert live trendbar", () => {
+    const result = T.toTrendbar(
+      {
+        volume: 10,
+        period: 1,
+        low: 976891000,
+        deltaOpen: 1000,
+        deltaHigh: 18000,
+        utcTimestampInMinutes: 26717050,
+      },
+      976904500
+    );
+    expect(result).toStrictEqual({
+      timestamp: 26717050 * 60000,
+      period: 1,
+      open: 976892000,
+      high: 976909000,
+      low: 976891000,
+      close: 976904500,
+      volume: 10,
+    });
+  });
+});
