a2f2333967cc5da61f5b200426c4b33da4dc885d
Claas Ahlrichs <claasahl@gmail.com>
2020-12-13 13:54:04 +0100

    updated experiment parameters

--- src/data/index.ts
+++ src/data/index.ts
@@ -28,7 +28,7 @@ const bt = (options: E.ExperimentBacktestOptions) =>
 
 // const symbol = "EURGBP";
 const symbol = "BTC/EUR";
-const fromDate = new Date("2020-10-30T00:00:00.000Z");
+const fromDate = new Date("2019-11-01T00:00:00.000Z");
 const toDate = new Date("2020-11-01T00:00:00.000Z");
 
 E.metrics(
