fd539f3b855770b50d4c023c25c27b0e811acef6
Claas Ahlrichs <claasahl@gmail.com>
2020-12-20 17:47:21 +0100

    updated experiment

--- src/data/index.ts
+++ src/data/index.ts
@@ -108,7 +108,7 @@ function currencies(...names: string[]): (data: SymbolData) => boolean {
 main({
   process: highLow({
     processSymbol: currencies("EURGBP"),
-    fromDate: new Date("2020-12-01T00:00:00.000Z"),
-    toDate: new Date(),
+    fromDate: new Date("2019-12-01T00:00:00.000Z"),
+    toDate: new Date("2020-12-01T00:00:00.000Z"),
   }),
 });
