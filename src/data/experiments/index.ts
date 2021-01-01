ae613cce98f374e436eb4ce0458e6ba6e8efd16a
Claas Ahlrichs <claasahl@gmail.com>
2021-01-01 14:46:23 +0100

    removed obsolete experiment

--- src/data/experiments/index.ts
+++ src/data/experiments/index.ts
@@ -2,6 +2,5 @@ export { run as highLow } from "./highLow";
 export { run as insideBarMomentum } from "./insideBarMomentum";
 export { run as metrics } from "./metrics";
 export { run as priceRange } from "./priceRange";
-export { run as recurringPriceLevels } from "./recurringPriceLevels";
 export * from "./types";
 export { run as vwap } from "./vwap";
