c8f5a5d1eb030764a815dba04b176a1ff81c951c
Claas Ahlrichs <claasahl@gmail.com>
2021-01-03 10:50:45 +0100

    removed old experiments

--- src/data/experiments/index.ts
+++
@@ -1,5 +0,0 @@
-export { run as highLow } from "./highLow";
-export { run as insideBarMomentum } from "./insideBarMomentum";
-export { run as priceRange } from "./priceRange";
-export * from "./types";
-export { run as vwap } from "./vwap";
