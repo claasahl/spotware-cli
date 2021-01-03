5bc22018104a0a02c7e09ef103d61b1f4118ce6e
Claas Ahlrichs <claasahl@gmail.com>
2021-01-03 11:18:43 +0100

    removed sample experiment

--- src/data/experiments/index.ts
+++ src/data/experiments/index.ts
@@ -1,4 +1,3 @@
 export { default as deals } from "./deals";
 export { default as highLow } from "./highLow";
 export { default as metrics } from "./metrics";
-export { default as sample } from "./sample";
