35bff16d64c8a26c06347e58dcda07d6dca3c192
Claas Ahlrichs <claasahl@gmail.com>
2021-04-07 10:55:41 +0200

    WIP attempt to skip already available tickdata

--- src/data/experiments/ticks.ts
+++ src/data/experiments/ticks.ts
@@ -17,6 +17,105 @@ type SaveTickDataOptions = {
   path: string;
 };
 
+type Index = {
+  symbolId: number;
+  symbolName: string;
+  fromTimestamp: number;
+  toTimestamp: number;
+  type: ProtoOAQuoteType;
+};
+
+type Period = {
+  fromTimestamp: number;
+  toTimestamp: number;
+  type: ProtoOAQuoteType;
+};
+
+async function mkdir(dir: string): Promise<void> {
+  try {
+    await fs.promises.mkdir(dir, { recursive: true });
+  } catch {
+    // ignore... for now
+  }
+}
+
+async function readPeriods(dir: string): Promise<Period[]> {
+  const data: Period[] = [];
+  const files = await fs.promises.readdir(dir);
+  for (const file of files) {
+    const text = Buffer.from(file, "base64").toString();
+    data.push(JSON.parse(text) as Index);
+  }
+  return data;
+}
+
+function removeOverlap(a: Period, b: Period): Period[] {
+  if (
+    a.type !== b.type ||
+    b.toTimestamp < a.fromTimestamp ||
+    a.toTimestamp < b.fromTimestamp
+  ) {
+    return [a];
+  }
+  const { type } = a;
+  if (a.fromTimestamp <= b.fromTimestamp && b.toTimestamp <= a.toTimestamp) {
+    // a completely engulfes b
+    return [
+      { fromTimestamp: a.fromTimestamp, toTimestamp: b.fromTimestamp, type },
+      { fromTimestamp: b.toTimestamp, toTimestamp: a.toTimestamp, type },
+    ];
+  }
+  if (b.fromTimestamp <= a.fromTimestamp && a.toTimestamp <= b.toTimestamp) {
+    // b completely engulfes a
+    return [];
+  }
+  if (
+    a.fromTimestamp < b.fromTimestamp &&
+    b.fromTimestamp < a.toTimestamp &&
+    a.toTimestamp <= b.toTimestamp
+  ) {
+    // a reaches into b
+    return [
+      { fromTimestamp: a.fromTimestamp, toTimestamp: b.fromTimestamp, type },
+    ];
+  }
+  if (
+    b.fromTimestamp < a.fromTimestamp &&
+    a.fromTimestamp < b.toTimestamp &&
+    b.toTimestamp <= a.toTimestamp
+  ) {
+    // b reaches into a
+    return [
+      { fromTimestamp: a.fromTimestamp, toTimestamp: b.fromTimestamp, type },
+    ];
+  }
+  return [a];
+}
+
+function removeOverlaps(
+  periods: Period[],
+  alreadyAvailable: Period[]
+): Period[] {
+  const data: Period[] = [...periods];
+
+  for (const available of alreadyAvailable) {
+    let index = 0;
+    while (index < data.length) {
+      const period = data[index];
+      console.log({ index, period, available });
+      const tmp = removeOverlap(period, available);
+
+      if (tmp.length === 1 && tmp[0] === period) {
+        // no change
+        index++;
+      } else {
+        data.splice(index, 1, ...tmp);
+      }
+    }
+  }
+  return data;
+}
+
 async function saveTickData(options: SaveTickDataOptions): Promise<void> {
   const step = 604800000;
   const { ctidTraderAccountId, symbolId, type } = options;
@@ -46,11 +145,11 @@ async function saveTickData(options: SaveTickDataOptions): Promise<void> {
       }
 
       const data = JSON.stringify(tickData);
-      const index = {
+      const index: Index = {
         symbolId: options.symbolId,
         symbolName: options.symbolName,
-        fromTimestamp: tickData[0].timestamp,
-        toTimestamp: tickData[tickData.length - 1].timestamp,
+        fromTimestamp: tickData[tickData.length - 1].timestamp,
+        toTimestamp: tickData[0].timestamp,
         type: options.type,
       };
       const filename =
@@ -73,36 +172,42 @@ function processor(options: Options): SymbolDataProcessor {
       return;
     }
 
+    const periods: Period[] = [
+      {
+        fromTimestamp: options.fromDate.getTime(),
+        toTimestamp: options.toDate.getTime(),
+        type: ProtoOAQuoteType.ASK,
+      },
+      {
+        fromTimestamp: options.fromDate.getTime(),
+        toTimestamp: options.toDate.getTime(),
+        type: ProtoOAQuoteType.BID,
+      },
+    ];
+
     // prepare dir
     const symbolName = data.symbol.symbolName?.replace("/", "") || "";
     const dir = `${symbolName}.DB`;
-    try {
-      await fs.promises.mkdir(dir, { recursive: true });
-    } catch {
-      // ignore... for now
-    }
+    await mkdir(dir);
+    const availablePeriods = await readPeriods(dir);
+
+    console.log({
+      availablePeriods,
+      periods,
+      tmp: removeOverlaps(periods, availablePeriods),
+    });
 
     // fetch data
-    await saveTickData({
-      socket,
-      ctidTraderAccountId: data.trader.ctidTraderAccountId,
-      symbolId: data.symbol.symbolId,
-      symbolName,
-      fromTimestamp: options.fromDate.getTime(),
-      toTimestamp: options.toDate.getTime(),
-      type: ProtoOAQuoteType.ASK,
-      path: dir,
-    });
-    await saveTickData({
-      socket,
-      ctidTraderAccountId: data.trader.ctidTraderAccountId,
-      symbolId: data.symbol.symbolId,
-      symbolName,
-      fromTimestamp: options.fromDate.getTime(),
-      toTimestamp: options.toDate.getTime(),
-      type: ProtoOAQuoteType.BID,
-      path: dir,
-    });
+    for (const period of periods) {
+      await saveTickData({
+        socket,
+        ctidTraderAccountId: data.trader.ctidTraderAccountId,
+        symbolId: data.symbol.symbolId,
+        symbolName,
+        path: dir,
+        ...period,
+      });
+    }
   };
 }
 export default processor;
