c01885d919acac0d157b8c0a759d0505309a4f40
Claas Ahlrichs <claasahl@gmail.com>
2021-04-13 17:21:08 +0200

    more tests

--- src/database/split.test.js
+++ src/database/split.test.js
@@ -6,6 +6,9 @@ const {
 describe("Database", () => {
   describe("retainAvailablePeriods", () => {
     test("no available periods, nothing to retain", () => {
+      //    period: ----
+      // available:
+      //  retained:
       const period = {
         fromTimestamp: 100,
         toTimestamp: 200,
@@ -14,16 +17,91 @@ describe("Database", () => {
       const retained = [];
       expect(retainAvailablePeriods(period, available)).toStrictEqual(retained);
     });
+    test("should retain available periods", () => {
+      //    period:  --------
+      // available: --  --  ----
+      //  retained:  ^  ^^  ^
+      const period = {
+        fromTimestamp: 100,
+        toTimestamp: 200,
+      };
+      const available = [
+        {
+          fromTimestamp: 80,
+          toTimestamp: 110,
+        },
+        {
+          fromTimestamp: 120,
+          toTimestamp: 140,
+        },
+        {
+          fromTimestamp: 170,
+          toTimestamp: 400,
+        },
+      ];
+      const retained = [
+        {
+          fromTimestamp: 100,
+          toTimestamp: 110,
+        },
+        {
+          fromTimestamp: 120,
+          toTimestamp: 140,
+        },
+        {
+          fromTimestamp: 170,
+          toTimestamp: 200,
+        },
+      ];
+      expect(retainAvailablePeriods(period, available)).toStrictEqual(retained);
+    });
   });
 
   describe("retainUnknownPeriods", () => {
     test("no available periods, retain initial input", () => {
+      //    period: ----
+      // available:
+      //  retained: ^^^^
       const period = {
         fromTimestamp: 100,
         toTimestamp: 200,
       };
       const available = [];
       const retained = [period];
+      expect(retainUnknownPeriods(period, available)).toStrictEqual(retained);
+    });
+    test("should retain unknown periods", () => {
+      //    period:  --------
+      // available: --  --  ----
+      //  retained:   ^^  ^^
+      const period = {
+        fromTimestamp: 100,
+        toTimestamp: 200,
+      };
+      const available = [
+        {
+          fromTimestamp: 80,
+          toTimestamp: 110,
+        },
+        {
+          fromTimestamp: 120,
+          toTimestamp: 140,
+        },
+        {
+          fromTimestamp: 170,
+          toTimestamp: 400,
+        },
+      ];
+      const retained = [
+        {
+          fromTimestamp: 110,
+          toTimestamp: 120,
+        },
+        {
+          fromTimestamp: 140,
+          toTimestamp: 170,
+        },
+      ];
       expect(retainUnknownPeriods(period, available)).toStrictEqual(retained);
     });
   });
