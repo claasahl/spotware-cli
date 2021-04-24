195c0c3978f91736679fa6ec9ff6d4d691ffa182
Claas Ahlrichs <claasahl@gmail.com>
2021-04-24 10:36:12 +0200

    fault tracing

--- src/waa.ts
+++ src/waa.ts
@@ -12,19 +12,46 @@ async function main() {
     ProtoOATrendbarPeriod.W1
   );
   console.log("---------------- srv");
-  console.log(
-    srv
-      .sort(DB.comparePeriod)
-      .map(forHumans)
-      .filter((_, i) => i <= 5)
-  );
+  for (const p of srv.sort(DB.comparePeriod).filter((_, i) => i <= 5)) {
+    const bars = await DB.readTrendbars(
+      "./SERVER/EURUSD.DB",
+      p,
+      ProtoOATrendbarPeriod.W1
+    );
+    const first = new Date(
+      (bars[0].utcTimestampInMinutes || 0) * 60000
+    ).toISOString();
+    const last = new Date(
+      (bars[bars.length - 1].utcTimestampInMinutes || 0) * 60000
+    ).toISOString();
+    console.log({
+      ...forHumans(p),
+      bars: bars.length,
+      first,
+      last,
+    });
+  }
+
   console.log("---------------- cli");
-  console.log(
-    cli
-      .sort(DB.comparePeriod)
-      .map(forHumans)
-      .filter((_, i) => i <= 5)
-  );
+  for (const p of cli.sort(DB.comparePeriod).filter((_, i) => i <= 5)) {
+    const bars = await DB.readTrendbars(
+      "./EURUSD.DB",
+      p,
+      ProtoOATrendbarPeriod.W1
+    );
+    const first = new Date(
+      (bars[0].utcTimestampInMinutes || 0) * 60000
+    ).toISOString();
+    const last = new Date(
+      (bars[bars.length - 1].utcTimestampInMinutes || 0) * 60000
+    ).toISOString();
+    console.log({
+      ...forHumans(p),
+      bars: bars.length,
+      first,
+      last,
+    });
+  }
 }
 
 main();
