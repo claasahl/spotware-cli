fe6445684f5e688e1417e8332d577cfc198c7947
Claas Ahlrichs <claasahl@gmail.com>
2021-04-24 18:58:55 +0200

    wip

--- src/client/strategies/institutionalCandles/structurePoints.ts
+++ src/client/strategies/institutionalCandles/structurePoints.ts
@@ -1,66 +1,57 @@
-function accumulate(data, candle, index, candles) {
-  if (!data.reference) {
-    data.reference = candle;
-    return data;
-  }
-  switch (data.direction) {
-    case "up": {
-      if (
-        data.reference.upper < candle.upper ||
-        data.reference.high < candle.high
-      ) {
-        data.reference = candle;
-        return data;
-      }
-      break;
-    }
-    case "down": {
-      if (
-        data.reference.low > candle.low ||
-        data.reference.lower > candle.lower
-      ) {
-        data.reference = candle;
-        return data;
-      }
-      break;
-    }
-  }
-  data.structurePoints.push({
-    ...data.reference,
-    direction: data.direction,
-    value: data.direction === "up" ? data.reference.high : data.reference.low,
-  });
-  data.reference = candle;
-  data.direction = data.direction === "up" ? "down" : "up";
-  return data;
-}
+import { upper, lower } from "indicators";
 
-export function structurePoints() {
-  const init = { reference: null, direction: "up", structurePoints: [] };
-  return shownCandles
-    .map((candle, index) => {
-      return {
-        index,
-        ...candle,
-        upper: Math.max(candle.open, candle.close),
-        lower: Math.min(candle.open, candle.close),
-      };
-    })
-    .reduce(accumulate, init)
-    .structurePoints.map((point, index, points) => {
-      const tmp = shownCandles.filter(
-        (c) =>
-          c.date > point.date &&
-          ((point.direction === "down" && c.low < point.value) ||
-            (point.direction === "up" && c.high > point.value))
-      );
-      if (tmp.length > 0) {
-        return {
-          mitigatedBy: tmp[0],
-          mitigated: tmp[0].date,
-          ...point,
-        };
+import { Trendbar } from "../../../utils";
+
+type StructurePoint = {
+  timestamp: number;
+  direction: "up" | "down";
+  value: number;
+  trendbar: Trendbar;
+};
+
+export function structurePoints(trendbars: Trendbar[]): StructurePoint[] {
+  const data: {
+    reference: Trendbar | null;
+    direction: "up" | "down";
+    structurePoints: StructurePoint[];
+  } = { reference: null, direction: "up", structurePoints: [] };
+  for (const trendbar of trendbars) {
+    if (!data.reference) {
+      data.reference = trendbar;
+      continue;
+    }
+
+    switch (data.direction) {
+      case "up": {
+        if (
+          upper(data.reference) < upper(trendbar) ||
+          data.reference.high < trendbar.high
+        ) {
+          data.reference = trendbar;
+          continue;
+        }
+        break;
       }
-      return point;
+      case "down": {
+        if (
+          data.reference.low > trendbar.low ||
+          lower(data.reference) > lower(trendbar)
+        ) {
+          data.reference = trendbar;
+          continue;
+        }
+        break;
+      }
+    }
+
+    data.structurePoints.push({
+      timestamp: data.reference.timestamp,
+      direction: data.direction,
+      value: data.direction === "up" ? data.reference.high : data.reference.low,
+      trendbar: data.reference,
     });
+    data.reference = trendbar;
+    data.direction = data.direction === "up" ? "down" : "up";
+  }
+  return data.structurePoints;
 }
