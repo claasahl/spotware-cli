8bab86cbb631ae994a90fb9eedb940f498a54ffd
Claas Ahlrichs <claasahl@gmail.com>
2021-04-24 12:53:05 +0200

    added test

--- src/database/read.test.js
+++ src/database/read.test.js
@@ -179,4 +179,82 @@ describe("Database", () => {
       }
     });
   });
+
+  describe("readTrendbarsChunk", () => {
+    const period = Object.freeze({
+      fromTimestamp: new Date("2021-02-14T22:00:00.000Z").getTime(),
+      toTimestamp: new Date("2021-04-01T00:00:00.000Z").getTime(),
+    });
+    const chunk = Object.freeze([
+      Object.freeze({
+        volume: 633406,
+        low: 120622,
+        deltaOpen: 584,
+        deltaClose: 101,
+        deltaHigh: 1807,
+        utcTimestampInMinutes:
+          new Date("2021-02-21T22:00:00.000Z").getTime() / 60000,
+      }),
+      Object.freeze({
+        volume: 627653,
+        low: 118927,
+        deltaOpen: 1773,
+        deltaClose: 179,
+        deltaHigh: 2203,
+        utcTimestampInMinutes:
+          new Date("2021-02-28T22:00:00.000Z").getTime() / 60000,
+      }),
+      Object.freeze({
+        volume: 568982,
+        low: 118355,
+        deltaOpen: 819,
+        deltaClose: 1179,
+        deltaHigh: 1544,
+        utcTimestampInMinutes:
+          new Date("2021-03-07T22:00:00.000Z").getTime() / 60000,
+      }),
+      Object.freeze({
+        volume: 471709,
+        deltaOpen: 721,
+        deltaClose: 302,
+        deltaHigh: 1150,
+        utcTimestampInMinutes:
+          new Date("2021-03-14T21:00:00.000Z").getTime() / 60000,
+      }),
+      Object.freeze({
+        volume: 465403,
+        low: 117618,
+        deltaOpen: 1257,
+        deltaClose: 318,
+        deltaHigh: 1849,
+        utcTimestampInMinutes:
+          new Date("2021-03-21T21:00:00.000Z").getTime() / 60000,
+      }),
+      Object.freeze({
+        volume: 352142,
+        low: 117040,
+        deltaOpen: 868,
+        deltaClose: 513,
+        deltaHigh: 898,
+        utcTimestampInMinutes:
+          new Date("2021-03-28T21:00:00.000Z").getTime() / 60000,
+      }),
+    ]);
+
+    test("should return complete chunk", async () => {
+      fs.promises.readdir.mockResolvedValue([
+        Buffer.from(JSON.stringify(period)).toString("base64") + ".json",
+      ]);
+      fs.promises.readFile.mockResolvedValue(JSON.stringify(chunk));
+      const trendbars = await readTrendbarsChunk(
+        "./EURUSD.DB",
+        {
+          fromTimestamp: new Date("2021-02-14T22:00:00.000Z").getTime(),
+          toTimestamp: new Date("2021-04-01T00:00:00.000Z").getTime(),
+        },
+        ProtoOATrendbarPeriod.H1
+      );
+      expect(trendbars).toStrictEqual(chunk);
+    });
+  });
 });
