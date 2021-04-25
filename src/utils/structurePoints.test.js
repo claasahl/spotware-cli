6a6a38620cce01ca967008af67c5ff4c4a9ef73f
Claas Ahlrichs <claasahl@gmail.com>
2021-04-25 10:08:14 +0200

    another test

--- src/utils/structurePoints.test.js
+++ src/utils/structurePoints.test.js
@@ -133,4 +133,67 @@ describe("Structure Points", () => {
       { timestamp: 600, direction: "down", value: 85, trendbar: barF },
     ]);
   });
+
+  test("upward ladder", () => {
+    //        F
+    //   C\  E
+    //  B   D
+    // A
+
+    const barA = Object.freeze({
+      timestamp: 600,
+      open: 90,
+      high: 100,
+      low: 85,
+      close: 98,
+      volumne: 0,
+    });
+    const barB = Object.freeze({
+      timestamp: 500,
+      open: 98,
+      high: 110,
+      low: 93,
+      close: 105,
+      volumne: 0,
+    });
+    const barC = Object.freeze({
+      timestamp: 400,
+      open: 105,
+      high: 120,
+      low: 105,
+      close: 118,
+      volumne: 0,
+    });
+    const barD = Object.freeze({
+      timestamp: 300,
+      open: 100,
+      high: 110,
+      low: 95,
+      close: 108,
+      volumne: 0,
+    });
+    const barE = Object.freeze({
+      timestamp: 200,
+      open: 108,
+      high: 120,
+      low: 103,
+      close: 115,
+      volumne: 0,
+    });
+    const barF = Object.freeze({
+      timestamp: 100,
+      open: 115,
+      high: 130,
+      low: 115,
+      close: 128,
+      volumne: 0,
+    });
+    const points = structurePoints([barA, barB, barC, barD, barE, barF]);
+    expect(points).toStrictEqual([
+      { timestamp: 100, direction: "down", value: 85, trendbar: barA },
+      { timestamp: 300, direction: "up", value: 120, trendbar: barC },
+      { timestamp: 400, direction: "down", value: 95, trendbar: barD },
+      { timestamp: 600, direction: "up", value: 130, trendbar: barF },
+    ]);
+  });
 });
