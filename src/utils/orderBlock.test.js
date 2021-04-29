559c1cde7cd6ced6dead9484888deb64b9adc55b
Claas Ahlrichs <claasahl@gmail.com>
2021-04-29 19:31:53 +0200

    added test

---
+++ src/utils/orderBlock.test.js
@@ -0,0 +1,19 @@
+const { structurePoints2 } = require("../../build/utils/structurePoints");
+const { orderBlocks } = require("../../build/utils/orderBlock");
+
+describe("order block", () => {
+  describe("real world examples", () => {
+    test("XAUUSD H1 2021-03-29", () => {
+      const bars = require("../../testdata/xauusd--h1--2021-03-29.json");
+      const points = structurePoints2(bars);
+      const blocks = orderBlocks(bars, points);
+
+      const ob = blocks.filter((ob) => ob.timestamp === 0)[0];
+      expect(ob).toStrictEqual(
+        expect.objectContaining({
+          timestamp: 0,
+        })
+      );
+    });
+  });
+});
