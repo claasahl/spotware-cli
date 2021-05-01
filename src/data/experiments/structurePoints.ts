69927c1a2f456849b7d0c657cc348b138fc84f90
Claas Ahlrichs <claasahl@gmail.com>
2021-05-01 17:03:44 +0200

    switched to NZDUSD

--- src/data/experiments/structurePoints.ts
+++ src/data/experiments/structurePoints.ts
@@ -64,8 +64,8 @@ async function fetchTrendbars(
 const periods = [
   // ProtoOATrendbarPeriod.W1,
   // ProtoOATrendbarPeriod.D1,
-  // ProtoOATrendbarPeriod.H4,
-  ProtoOATrendbarPeriod.H1,
+  ProtoOATrendbarPeriod.H4,
+  // ProtoOATrendbarPeriod.H1,
   // ProtoOATrendbarPeriod.M30,
   // ProtoOATrendbarPeriod.M5,
 ];
--- src/data/index.ts
+++ src/data/index.ts
@@ -22,15 +22,15 @@ async function main() {
   const processSymbol = currencies(
     // "EURGBP",
     // "EURUSD"
-    "AUDUSD"
+    "NZDUSD"
     // "EURAUD",
     // "EURNZD",
     // "EURJPY",
     // "EURCHF",
     // "EURCAD"
   );
-  const fromDate = new Date("2021-01-01T00:00:00.000Z");
-  const toDate = new Date("2021-02-01T00:00:00.000Z");
+  const fromDate = new Date("2021-03-01T00:00:00.000Z");
+  const toDate = new Date("2021-04-01T00:00:00.000Z");
   await run({
     process: E.structurePoints({
       fromDate,
